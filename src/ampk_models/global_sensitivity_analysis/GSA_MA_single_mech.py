946b6a64005f08bd0e932e6224aa0d65c51db8bb
Nate Linden <natejlinden@gmail.com>
2023-04-24 20:22:13 -0700

    code to run MA single model

--- src/ampk_models/global_sensitivity_analysis/GSA_MA_single_mech.py
+++ src/ampk_models/global_sensitivity_analysis/GSA_MA_single_mech.py
@@ -1,42 +1,59 @@
-import sunode
-import matplotlib.pyplot as plt
-import matplotlib as mpl
 import numpy as np
-import sympy as sym
 from SALib.sample import sobol as sobol_samp
 from SALib.sample import morris as morris_samp
 from SALib.analyze import sobol as sobol_analyze
 from SALib.analyze import morris as morris_analyze
 from SALib.analyze.hdmr import analyze as hdmr_analyze
-import sys
 import os
-import time
+import sys
 import multiprocessing as mp
-from tqdm import tqdm
-from joblib import Parallel, delayed
-sys.path.insert(0, '../odes')
+import time
+import math
 
-import ampk_MA_single_mech as model1
+# get user inputs
+dir = sys.argv[1]
+cpu_mult = int(sys.argv[2])
+
+# use all available cores (do before loading jax)
+# required for parallel cpu runs
+n_cores = mp.cpu_count()
+print('Using {} cores'.format(n_cores))
+n_devices = int(2**np.ceil(math.log(cpu_mult*n_cores, 2))) # sets n_devices to the next largest power of 2
+print('Set {} XLA devices'.format(n_devices))
+
+xla_flag = '--xla_force_host_platform_device_count={}'.format(n_devices)
+os.environ['XLA_FLAGS']=xla_flag
+
+import jax
+import jax.numpy as jnp
+from jax import lax
+import equinox as eqx
+import diffrax as dfrx
+
+# load code
+sys.path.insert(0, '../odes')
+import ampk_MA_single_mech_diffrax as model
 from gsa_utils import *
 
+jax.config.update('jax_enable_x64', True)
+jax.config.update('jax_platform_name', 'cpu')
+print(len(jax.devices()))
 ############################################
 # Setup output directory #
 ############################################
-dir = sys.argv[1]
 print('Saving to: ', dir)
 
-fname = 'MA_single_mech'
+fname = 'MA_single_mech/'
 savedir = dir+fname
 if not os.path.exists(savedir):
     os.makedirs(savedir)
     print('Created directory: ', savedir)
 
-
 ############################################
 # Bounds and info for params #
 ############################################
 
-# define the bounds for the parameters
+# define the bounds for the AMPK parameters
 # we use plus or minus on order of magnitude of any known values and then make reasonable assumptions for unknowns
 # Note we fix all off rates to 1.0 and dont bother sampling these or computing sensitivities
 ############################################
@@ -58,12 +75,63 @@ nominal_vals_MA = [
    1e-3, # AMPKAR
 ]
 
-bounds_MA_90 = [[0.1*param, 1.9*param] for param in nominal_vals_MA]
+# metabolism_params
+metab_parms_basal = {'kGly': 1300.0,'kHydro':1.4e-3,'kForAK':40.44,
+                     'kRevAK':1.1e-3,'VmaxOxPhos':0.5,'Kadp': 5.8e-2,'n': 2.568,}
+metab_parms_stress = {'kGly': 4.5,'kHydro':1.4e-3,'kForAK':40.44,
+                      'kRevAK':1.1e-3,'VmaxOxPhos':0.5,'Kadp': 5.8e-2,'n': 2.568,}
+
+lb_mult = 0.5
+ub_mult = 1.5
+bounds_MA = [[lb_mult*param, ub_mult*param] for param in nominal_vals_MA]
 
 param_names_MA = ['KdAMP', 'KdADP', 'KdATP', 'kOnCaMKK', 'kPhosCaMKK', 
                'kOnLKB1','kPhosLKB1','kOnPP','kDephosPP', 
                'kOnAMPK','kPhosAMPK', 'kOnPP1','kDephosPP1', 'AMPKAR']
-state_names = ['AMP', 'ADP', 'ATP', 'AMPK', 'pAMPK', 'AMP_AMPK', 'ADP_AMPK', 'ATP_AMPK', 'AMP_pAMPK', 'ADP_pAMPK', 'ATP_pAMPK', 'CaMKK', 'CaMKK_AMPK', 'CaMKK_AMP_AMPK', 'CaMKK_ADP_AMPK', 'CaMKK_ATP_AMPK', 'LKB1', 'LKB1_AMP_AMPK', 'LKB1_ADP_AMPK', 'PP', 'PP_pAMPK', 'PP_ATP_pAMPK', 'AMPKAR', 'pAMPKAR', 'AMPKAR_AMP_pAMPK', 'PP1', 'PP1_pAMPKAR']
+state_names = ['AMP', 'ADP', 'ATP', 'AMPK', 'pAMPK', 'AMP_AMPK', 'ADP_AMPK', 'ATP_AMPK', 'AMP_pAMPK', 'ADP_pAMPK', 'ATP_pAMPK', 'CaMKK', 'CaMKK_AMPK', 'CaMKK_AMP_AMPK', 'CaMKK_ADP_AMPK', 'CaMKK_ATP_AMPK', 'LKB1', 'LKB1_AMP_AMPK', 'LKB1_ADP_AMPK', 'PP', 'PP_pAMPK', 'PP_ATP_pAMPK','AMPKAR', 'pAMPKAR', 'AMPKAR_AMP_pAMPK', 'PP1', 'PP1_pAMPKAR']
+
+# initial conditions
+to_set = ['AMP', 'ADP', 'ATP', 'AMPK', 'CaMKK', 'LKB1', 'PP', 'AMPKAR', 'PP1']
+idxs = [state_names.index(item) for item in to_set]
+
+print(idxs)
+
+y0 = np.zeros((53,))
+y0[idxs[0]] = 2e-5   # 'AMP' mM
+y0[idxs[1]] = 1.3e-1 # 'ADP mM
+y0[idxs[2]] = 8.2   # 'ATP mM
+y0[idxs[3]] = 0.6   # 'AMPK mM
+y0[idxs[4]] = 1.0   # 'CaMKK_AMPK mM
+y0[idxs[5]] = 1.0   # 'CaMKK_AMPK mM
+y0[idxs[6]] = 1.0   # 'CaMKK_AMPK mM
+y0[idxs[7]] = 0.1   # 'AMPKAR mM
+y0[idxs[8]] = 1.0   # 'CaMKK_AMPK mM
+
+y0 = np.array(y0)
+
+# function to compute all MA params from sampled params
+def compute_MA_params(params):
+    return (1.0, # kOnAMP
+            params[0], # kOffAMP
+            1.0 , # kOnADP
+            params[1], # kOffADP
+            1.0, # kOnATP
+            params[2], # kOffATP
+            params[3], # kOnCaMKK
+            1.0, # kOffCaMKK
+            params[4], # kPhosCaMKK
+            params[5], # kOnLKB1
+            1.0, # kOffLKB1
+            params[6], # kPhosLKB1
+            params[7], # kOnPP
+            1.0, # kOffPP
+            params[8], # kDephosPP
+            params[9], # kOnAMPK
+            1.0, # kOffAMPK
+            params[10], # kPhosAMPK
+            params[11], # kOnPP1
+            1.0, # kOffPP1
+            params[12]) # kDephosPP1
 
 ############################################
 # We also want to sample Michaelis-Menten parameters to generate correlated samples
@@ -80,69 +148,107 @@ nominal_vals_MM = [
    6.7e-2, # KmPP
    6.33, # kPhosAMPK
    4.67e-3, # KmAMPK
+
    1.1e-1, # kDephosPP1
    6.7e-2, # KmPP1
    1e-3, # AMPKAR
 ]
 
-bounds_MM_90 = [[0.1*param, 1.9*param] for param in nominal_vals_MM]
-
+bounds_MM = [[lb_mult*param, ub_mult*param] for param in nominal_vals_MM]
 param_names_MM = ['KdAMP', 'KdADP', 'KdATP', 'kPhosCaMKK', 'KmCamKK', 'kPhosLKB1', 'KmLKB1', 'kDephosPP', 'KmPP', 'kPhosAMPK', 'KmAMPK', 
                'kDephosPP1', 'KmPP1', 'AMPKAR']
 
 # dictionary of the problem for SALib
-problem_MA_90 = {'num_vars':14, 'names':param_names_MA, 'bounds': bounds_MA_90,}
-problem_MM_90 = {'num_vars':14, 'names':param_names_MM, 'bounds': bounds_MM_90,}
+bounds_MA = {'num_vars':14, 'names':param_names_MA, 'bounds': bounds_MA,}
+bounds_MM = {'num_vars':14, 'names':param_names_MM, 'bounds': bounds_MM,}
 
-
+# random seed for reproducibility
 seed = np.random.seed(seed=2048)
 
+#####################################################################
+# SET UP Jitable functions to solve to steady-state and compute qoi #
+#####################################################################
+## Now test a solve to steady-state function
+# jitable function to run the system to steady-state
+@jax.jit
+def solve_to_steady_state(params, rhs, y0, thresh=1e-10):
+    solver=dfrx.Kvaerno5()
+    stepsize_controller = dfrx.PIDController(rtol=1e-8, atol=1e-8)
+    t0 = 0.0
+    t1 = 1000.0 # 1000 seconds
+    times = np.arange(t0, t1, t1/100)
+    dt0 = 1e-6 # initial time step
+    saveat=dfrx.SaveAt(ts=times)
+    times_repeat = np.arange(t0, 100, 100/100)
+    saveat_repeat=dfrx.SaveAt(ts=times_repeat)
 
-############################################
-# SET UP SOLVER AND MODEL #
-############################################
-# load the necessary dictionaries
-params = model1.ampk_MA_single_mech_get_params()
-states = model1.ampk_MA_single_mech_get_states()
-nparams = len(params)
-nstates = len(states)
+    # initial solve
+    sol = dfrx.diffeqsolve(
+        rhs, 
+        solver, 
+        t0, t1, dt0, 
+        y0, 
+        saveat=saveat, stepsize_controller=stepsize_controller,
+        args=params)
 
-# create sunode problem
-problem_ode = sunode.SympyProblem(
-    params=params,
-    states=states,
-    rhs_sympy=model1.ampk_MA_single_mech_RHS,
-    # specify variables to take gradients wrt
-    derivative_params=(),
-)
+    # solve to steady-state
+    # cond_fun checks for steady-state by checking the norm of the RHS
+    cond_fun = lambda sol: jnp.linalg.norm(rhs.vector_field(0.0, sol[0].ys[-1,:], params)) > thresh
+    # body_fun solves the ODEs for 100 more seconds
+    body_fun = lambda sol: (dfrx.diffeqsolve(rhs, solver, t0, 100, dt0, sol[0].ys[-1,:], 
+        saveat=saveat_repeat, stepsize_controller=stepsize_controller, args=params),
+        sol[1]+100)
+    
+    # while loop until steady state condition is reached
+    sol_final = lax.while_loop(cond_fun, body_fun, (sol, 1000))
+    
+    return sol_final # returns the final state
 
-# solver
-solver = sunode.solver.Solver(problem_ode, solver='BDF')
-lib = sunode._cvodes.lib
-lib.CVodeSetMaxNumSteps(solver._ode, 2500) # increase max steps
+# jitable function to compute the qois
+@jax.jit
+def single_model_eval(params, rhs_basal, rhs_stress, y0, ampkar_idx=46, pampkar_idx=47):
+    # update y0 for AMPKAR
+    y0 = y0.at[ampkar_idx].set(params[-1])
 
-y0 = set_init_conds(problem_ode, state_names)
+    # find basal steady-state
+    sol_basal = solve_to_steady_state(params, rhs_basal, y0, 1e-12)
 
- # evaluate the solution at these times
-tvals = np.linspace(0, 1000, 2000)
+    # apply stimulus and run again
+    sol_stress = solve_to_steady_state(params, rhs_stress, sol_basal[0].ys[-1,:], 1e-10)
 
+    # compute the ratio of pAMPKAR/AMPKAR at the end of the stress simulation
+    basal_ratio = sol_basal[0].ys[-1,pampkar_idx]/sol_basal[0].ys[-1,ampkar_idx]
+    stress_ratio = sol_stress[0].ys[-1,pampkar_idx]/sol_stress[0].ys[-1,ampkar_idx]
+    norm_change = (stress_ratio - basal_ratio) / basal_ratio
+    
+    return jnp.array([norm_change, basal_ratio, stress_ratio, sol_basal[1], sol_stress[1]])
 
-############################################
-# Test Case with small number of samples #
-############################################
-# generate samples using the Saltelli and Morris sampling methods
-# START with a small number of samples
-nsamps = 8
-param_vals_sobol_MA = sobol_samp.sample(problem_MA_90, nsamps, calc_second_order=False, seed=seed)
-param_vals_morris_MA = morris_samp.sample(problem_MA_90, nsamps, seed=seed)
-param_vals_sobol_MM = sobol_samp.sample(problem_MM_90, nsamps, calc_second_order=False, seed=seed)
-param_vals_morris_MM = morris_samp.sample(problem_MM_90, nsamps, seed=seed)
+@jax.jit
+def single_model_eval_nansafe(params, rhs_basal, rhs_stress, y0):
+    pred = jnp.sum(jnp.isnan(params))
+    false_fun = lambda params: single_model_eval(params, rhs_basal, rhs_stress, y0)
+    true_fun = lambda params: jnp.array([jnp.nan, jnp.nan, jnp.nan, jnp.nan, jnp.nan])
+    return lax.cond(pred, true_fun, false_fun, params)
+
+################################################
+#                   Model RHS                  #
+################################################
+rhs = model.ampk_MA_single_mech(**metab_parms_basal)
+rhs_stress = model.ampk_MA_single_mech(**metab_parms_stress)
+rhs = dfrx.ODETerm(rhs)
+rhs_stress = dfrx.ODETerm(rhs_stress)
+
+################################################
+# Full scale case with large number of samples #
+################################################
+# generate samples using the Sobol sampling method
+nsamps = 1024
+param_vals_sobol_MA = sobol_samp.sample(bounds_MA, nsamps, calc_second_order=True, seed=seed)
+param_vals_sobol_MM = sobol_samp.sample(bounds_MM, nsamps, calc_second_order=True, seed=seed)
 
 # copy MA to np arrays
 param_vals_sobol_MA_corr = np.array(param_vals_sobol_MA)
-param_vals_morris_MA_corr = np.array(param_vals_morris_MA)
 param_vals_sobol_MM_np = np.array(param_vals_sobol_MM)
-param_vals_morris_MM_np = np.array(param_vals_morris_MM)
 
 # now compute all kons using samples of Km and from the MM model and Kcat from MA model
 kcat_idxs_MA = [4,6,8,10,12]
@@ -150,106 +256,42 @@ kon_idxs_MA = [3,5,7,9,11]
 km_idxs_MM = [4,6,8,10,12]
 for kcat_i, kon_i, km_i in zip(kcat_idxs_MA, kon_idxs_MA, km_idxs_MM):
     param_vals_sobol_MA_corr[:,kon_i] = (1+param_vals_sobol_MA_corr[:,kcat_i])/param_vals_sobol_MM_np[:,km_i]
-for kcat_i, kon_i, km_i in zip(kcat_idxs_MA, kon_idxs_MA, km_idxs_MM):  
-    param_vals_morris_MA_corr[:,kon_i] = (1+param_vals_morris_MA_corr[:,kcat_i])/param_vals_morris_MM_np[:,km_i]
-
-# Run simulations
-y0 = set_init_conds(problem_ode, state_names)
-num_jobs=mp.cpu_count()
-
-#with Parallel(n_jobs=num_jobs) as parallel:
-    # uncorrelated
-    #param_vals_sobol_MA= tqdm(param_vals_sobol_MA)
-    #sols_sobol = parallel(delayed(single_model_eval)(param, 
-    #                        problem_ode, solver, y0, tvals, state_names) for 
-    #                        param in param_vals_sobol_MA)
-    #np.save(savedir + 'sols_sobol_short.npy', np.array(sols_sobol, dtype=object))
-    #del sols_sobol
-    #param_vals_morris_MA= tqdm(param_vals_morris_MA)
-    #sols_morris = parallel(delayed(single_model_eval)(param, 
-    #                        problem_ode, solver, y0, tvals, state_names) for 
-    #                        param in param_vals_morris_MA)
-    #np.save(savedir + 'sols_morris_short.npy', np.array(sols_morris, dtype=object))
-    #del sols_morris
-    # correlated
-    #param_vals_sobol_MA_corr= tqdm(param_vals_sobol_MA_corr)
-    #sols_sobol_corr = parallel(delayed(single_model_eval)(param, 
-    #                        problem_ode, solver, y0, tvals, state_names) for 
-    #                        param in param_vals_sobol_MA_corr)
-    #np.save(savedir + 'sols_sobol_corr_short.npy', np.array(sols_sobol_corr, dtype=object))
-    #del sols_sobol_corr
-    #param_vals_morris_MA_corr= tqdm(param_vals_morris_MA_corr)
-    #sols_morris_corr = parallel(delayed(single_model_eval)(param, 
-    #                        problem_ode, solver, y0, tvals, state_names) for 
-    #                        param in param_vals_morris_MA_corr)
-    #np.save(savedir + 'sols_morris_corr_short.npy', np.array(sols_morris_corr, dtype=object))
-    #del sols_morris_corr
-
-############################################
-# Full scale case with many samples #
-############################################
-# generate samples using the Saltelli and Morris sampling methods
-# START with a small number of samples
-nsamps = 2048
-param_vals_sobol_MA = sobol_samp.sample(problem_MA_90, nsamps, calc_second_order=True, seed=seed)
-param_vals_morris_MA = morris_samp.sample(problem_MA_90, nsamps, seed=seed)
-param_vals_sobol_MM = sobol_samp.sample(problem_MM_90, nsamps, calc_second_order=True, seed=seed)
-param_vals_morris_MM = morris_samp.sample(problem_MM_90, nsamps, seed=seed)
-
-np.save(savedir + 'param_vals_sobol_MA.npy', np.array(param_vals_sobol_MA))
-np.save(savedir + 'param_vals_morris_MA.npy', np.array(param_vals_morris_MA))
-
-# copy MA to np arrays
-param_vals_sobol_MA_corr = np.array(param_vals_sobol_MA)
-param_vals_morris_MA_corr = np.array(param_vals_morris_MA)
-param_vals_sobol_MM_np = np.array(param_vals_sobol_MM)
-param_vals_morris_MM_np = np.array(param_vals_morris_MM)
-
-# now compute all kons using samples of Km and from the MM model and Kcat from MA model
-kcat_idxs_MA = [4,6,8,10,12]
-kon_idxs_MA = [3,5,7,9,11]
-km_idxs_MM = [4,6,8,10,12]
-for kcat_i, kon_i, km_i in zip(kcat_idxs_MA, kon_idxs_MA, km_idxs_MM):
-    param_vals_sobol_MA_corr[:,kon_i] = (1+param_vals_sobol_MA_corr[:,kcat_i])/param_vals_sobol_MM_np[:,km_i]
-for kcat_i, kon_i, km_i in zip(kcat_idxs_MA, kon_idxs_MA, km_idxs_MM):  
-    param_vals_morris_MA_corr[:,kon_i] = (1+param_vals_morris_MA_corr[:,kcat_i])/param_vals_morris_MM_np[:,km_i]
 
 np.save(savedir + 'param_vals_sobol_MA_corr.npy', np.array(param_vals_sobol_MA_corr))
-np.save(savedir + 'param_vals_morris_MA_corr.npy', np.array(param_vals_morris_MA_corr))
 
 # Run simulations
-y0 = set_init_conds(problem_ode, state_names)
-num_jobs=mp.cpu_count()
+print('Reshaping input parameters...')
+# qoi_fn = lambda params: single_model_eval(params, rhs, rhs_stress, y0)
+# qoi_fn_vmap = jax.vmap(single_model_eval, in_axes=(0,None,None,None))
+qoi_fn_pmap = jax.pmap(single_model_eval_nansafe, in_axes=(0,None,None,None))
 
-with Parallel(n_jobs=num_jobs) as parallel:
-    # Sobol
-    # uncorrelated
-    param_vals_sobol_MA= tqdm(param_vals_sobol_MA)
-    sols_sobol = parallel(delayed(single_model_eval)(param, 
-                            problem_ode, solver, y0, tvals, state_names, full_output=False) for 
-                            param in param_vals_sobol_MA)
-    np.save(savedir + 'sols_sobol.npy', np.array(sols_sobol))
-    del sols_sobol
-    # correlated
-    param_vals_sobol_MA_corr= tqdm(param_vals_sobol_MA_corr)
-    sols_sobol_corr = parallel(delayed(single_model_eval)(param, 
-                            problem_ode, solver, y0, tvals, state_names, full_output=False) for 
-                            param in param_vals_sobol_MA_corr)
-    np.save(savedir + 'sols_sobol_corr.npy', np.array(sols_sobol_corr))
-    del sols_sobol_corr
+params_shape = param_vals_sobol_MA_corr.shape # get shape of parameter vectors (n_sampls, n_params)
+# the parameter vector needs to be shape (n_loops, n_devices, n_params)
+# n_loops needs to be the integer which is larger than n_sampls//n_devices
+# thus we need to pad any extra entries added with nans
+# for example if we have 120 parameter set, then we need to add 8 row of nans
+pad = int((np.ceil(params_shape[0]/n_devices)*n_devices)-params_shape[0]) #params_shape[0] % n_devices
+if pad:
+    pad_mat = np.empty((pad, params_shape[1]))
+    pad_mat[:] = np.nan
+    param_vals_sobol_MA_corr = np.vstack((param_vals_sobol_MA_corr, pad_mat))
 
-    # Morris
-    #uncorrelated
-    param_vals_morris_MA= tqdm(param_vals_morris_MA)
-    sols_morris = parallel(delayed(single_model_eval)(param, 
-                            problem_ode, solver, y0, tvals, state_names, full_output=False) for 
-                            param in param_vals_morris_MA)
-    np.save(savedir + 'sols_morris.npy', np.array(sols_morris))
-    del sols_morris
-    #correlated
-    param_vals_morris_MA_corr= tqdm(param_vals_morris_MA_corr)
-    sols_morris_corr = parallel(delayed(single_model_eval)(param, 
-                            problem_ode, solver, y0, tvals, state_names, full_output=False) for 
-                            param in param_vals_morris_MA_corr)
-    del sols_morris_corr
-    np.save(savedir + 'sols_morris_corr.npy', np.array(sols_morris_corr))
+# now we can reshape the parameter vector accordingly
+n_loops = int(np.ceil(params_shape[0]/n_devices))
+new_params = jnp.array(param_vals_sobol_MA_corr).reshape((n_loops,n_devices,params_shape[1]))
+
+# we are now ready to run simulations
+print('Running simulations...')
+sols_sobol_MA =[]
+tnow = time.time()
+for i in range(n_loops):
+    sol = qoi_fn_pmap(new_params[i,:,:], rhs, rhs_stress, y0)
+    sols_sobol_MA.append(sol)
+tend = time.time()
+
+print('Simulations took {} seconds'.format(tend-tnow))
+print('Saving results...')
+np.save(savedir + 'sols_sobol_corr.npy', jnp.array(sols_sobol_MA))
+
+print('Complete!')
+quit()
---
+++ src/ampk_models/odes/ampk_MA_single_mech_diffrax.py
@@ -0,0 +1,171 @@
+"""
+    Nathaniel Linden (UCSD MAE)
+    Created: April 24th, 2023
+
+    This file contains the functions for a model of MAPK activation. That model makes the 
+    following high-level assumptions:
+        - allow double adenine nucleotide AMPK binding
+        - use only mass action kinetics
+        - the reaction mechanism reflects specific activation and inhibition of
+            AMPK and phos/dephos by AXPs
+
+    The get_params functions returns a dictionary of the parameters in the correct
+    format.
+
+    The get_states function returns a dictionary od the parameters in the correct
+    format.
+
+    The RHS function is written following the syntax specified for the pymc/sunode
+    package. See docs here https://sunode.readthedocs.io/en/latest/without_pymc.html
+"""
+import jax.numpy as jnp
+import equinox as eqx
+
+class ampk_MA_single_mech(eqx.Module):
+    """Right hand side of the AMPK_ma_double_mech regulation model.
+
+    Written in the format required by the diffrax package
+    """
+
+    # fixed parameters
+    # metabolic params
+    kGly: float
+    kHydro: float
+    kForAK: float
+    kRevAK: float
+    VmaxOxPhos: float
+    Kadp: float
+    n: float
+
+
+    def __init__(self, kGly, kHydro, kForAK, kRevAK, VmaxOxPhos, Kadp, n):
+        """Initialize the model. Set fixed parameters."""
+        # TODO: expand docstring
+        # TODO: add default values
+        self.kGly = kGly
+        self.kHydro = kHydro
+        self.kForAK = kForAK
+        self.kRevAK = kRevAK
+        self.VmaxOxPhos = VmaxOxPhos
+        self.Kadp = Kadp
+        self.n = n
+
+    def __call__(self, t, y, args):
+        """Right hand side of the AMPK_ma_single_mech regulation model.
+
+        Written in the format required by the diffrax package
+        """
+        # unpack parameters
+        kOnAMP      = args[0] # AMP binding
+        kOffAMP     = args[1]
+        kOnADP      = args[2] # ADP binding
+        kOffADP     = args[3]
+        kOnATP      = args[4] # ATP binding
+        kOffATP     = args[5]
+        kOnCaMKK    = args[6] # CaMKK binding
+        kOffCaMKK   = args[7]
+        kPhosCaMKK  = args[8] # phosphorylation
+        kOnLKB1     = args[9] # LKB1 binding
+        kOffLKB1    = args[10]
+        kPhosLKB1   = args[11] # phosphorylation
+        kOnPP       = args[12] # Phosphatase AMPK binding
+        kOffPP      = args[13]
+        kDephosPP   = args[14] # dephosphorylation
+        kOnAMPK     = args[15] # pAMPK binds AMPKAR
+        kOffAMPK    = args[16]
+        kPhosAMPK   = args[17] # pAMPK phosphorylates AMPKAR
+        kOnPP1      = args[18] # Phosphatase AMPKAR binding
+        kOffPP1     = args[19] 
+        kDephosPP1  = args[20]
+
+        # FLUXES
+        # single AXP complexing
+        # single AXP complexing
+        J1 = kOnAMP*y[0]*y[3] - kOffAMP*y[5] # AMPK
+        J2 = kOnADP*y[1]*y[3] - kOffADP*y[6]
+        J3 = kOnATP*y[2]*y[3] - kOffATP*y[7]
+        J4 = kOnAMP*y[0]*y[4] - kOffAMP*y[8] # pAMPK
+        J5 = kOnADP*y[1]*y[4] - kOffADP*y[9]
+        J6 = kOnATP*y[2]*y[4] - kOffATP*y[10]
+        # CaMKK complexing and phosphorylation
+        J7 = kOnCaMKK*y[11]*y[3] - kOffCaMKK*y[12]
+        J8 = kPhosCaMKK*y[12]
+        J9 = kOnCaMKK*y[11]*y[5] - kOffCaMKK*y[13]  
+        J10 = kPhosCaMKK*y[13] 
+        J11 = kOnCaMKK*y[11]*y[6] - kOffCaMKK*y[14]
+        J12 = kPhosCaMKK*y[14]
+        J13 = kOnCaMKK*y[11]*y[7] - kOffCaMKK*y[15]
+        J14 = kPhosCaMKK*y[15]
+        # LKB1 complexing and phosphorylation
+        J15 = kOnLKB1*y[16]*y[5] - kOffLKB1*y[17]
+        J16 = kPhosLKB1*y[17]
+        J17 = kOnLKB1*y[16]*y[6] -  kOffLKB1*y[18]
+        J18 = kPhosLKB1*y[18]
+        # phosphatase binding and dephosphorylation
+        J19 = kOnPP*y[19]*y[4] - kOffPP*y[20]
+        J20 =  kDephosPP*y[20]
+        J21 = kOnPP*y[19]*y[10] - kOffPP*y[21]
+        J22 =  kDephosPP*y[21]
+        # AMPK binding to AMPAKAR and phosphorylation
+        J23 = kOnAMPK*y[22]*y[8] - kOffAMPK*y[24]
+        J24 = kPhosAMPK*y[24]
+        # PP1 binding to AMPKAR and dephosphorylation  
+        J25 = kOnPP1*y[25]*y[23] - kOffPP1*y[26]
+        J26 = kDephosPP1*y[26]
+        # Metabolic fluxes
+        # glycolysis
+        Jgly = 2*self.kGly*y[1]*y[1]
+        # ATP hydrolysis
+        Jhydro = self.kHydro*y[2]
+        # Adenylate Kinase
+        Jak = (self.kForAK*y[2]*y[0]) - (self.kRevAK*y[1]*y[1]) # MASS ACTION KINETICS!
+        # Oxidative Phos
+        Joxphos = (self.VmaxOxPhos * ((y[1]/self.Kadp)**self.n))/(1 + ((y[1]/self.Kadp)**self.n))
+
+
+
+        # now return the odes for each state variable
+        dydt = jnp.zeros((27,)) # 53 state variables jax array
+        dydt = dydt.at[0].set(-J1-J4-Jak)
+        dydt = dydt.at[1].set(-J2-J5-Jgly+2*Jak+Jhydro-Joxphos)
+        dydt = dydt.at[2].set(-J3-J6+Jgly-Jak-Jhydro+Joxphos)
+        # free AMPK
+        dydt = dydt.at[3].set(-J1-J2-J7+J20)
+        dydt = dydt.at[4].set(-J4-J5-J6+J8-J19)
+        # single AXP-AMPK complexes
+        dydt = dydt.at[5].set(J1-J9-J15)
+        dydt = dydt.at[6].set(J2-J11-J17)
+        dydt = dydt.at[7].set(J3-J13+J22)
+        # single AXP-pAMPK complexes
+        dydt = dydt.at[8].set(J4+J10+J16-J23+J24)
+        dydt = dydt.at[9].set(J5+J12+J18)
+        dydt = dydt.at[10].set(J6+J14-J21)
+        # CaMKK complexes
+        dydt = dydt.at[11].set(-J7+J8-J9+J10-J11+J12-J13+J14)
+        dydt = dydt.at[12].set(J7-J8)
+        dydt = dydt.at[13].set(J9-J10)
+        dydt = dydt.at[14].set(J11-J12)
+        dydt = dydt.at[15].set(J13-J14)
+        # LKB1 complexes
+        dydt = dydt.at[16].set(-J15+J16-J17+J18)
+        dydt = dydt.at[17].set(J15-J16)
+        dydt = dydt.at[18].set(J17-J18)
+        # AMPK phosphatase complexes
+        dydt = dydt.at[19].set(-J19+J20-J21+J22)
+        dydt = dydt.at[20].set(J19-J20)
+        dydt = dydt.at[21].set(J21-J22)
+        # free AMPKAR
+        dydt = dydt.at[22].set(-J23+J26)
+        dydt = dydt.at[23].set(J24-J25)
+        # AMPKAR-pAMPK complexes
+        dydt = dydt.at[24].set(J23-J24)
+        # AMPKAR phosphatase complexes
+        dydt = dydt.at[25].set(-J25+J26)
+        dydt = dydt.at[26].set(J25-J26)
+
+        return dydt
+
+
+    def set_kGly(self, kGly):
+            """Set the glycolysis rate parameter."""
+            self.kGly = kGly
