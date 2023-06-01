6ebab94fa5b71ac5201fc232b4c85db7b8707504
Nathaniel Linden <nlinden@ucsd.edu>
2023-06-01 16:07:34 -0700

    fixed tqdm import

--- src/ampk_models/global_sensitivity_analysis/GSA_MA_double_mech.py
+++ src/ampk_models/global_sensitivity_analysis/GSA_MA_double_mech.py
@@ -4,7 +4,7 @@ from SALib.sample import morris as morris_samp
 from SALib.analyze import sobol as sobol_analyze
 from SALib.analyze import morris as morris_analyze
 from SALib.analyze.hdmr import analyze as hdmr_analyze
-import tdqm
+from tqdm import tdqm
 import os
 import sys
 import multiprocessing as mp
