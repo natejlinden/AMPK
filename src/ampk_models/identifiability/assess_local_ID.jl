3f2fd3df05a6ad35e6f0702d85b11085eab72e0e
Nathaniel Linden <nlinden@ucsd.edu>
2023-06-04 17:40:45 -0700

    test all params

--- src/ampk_models/identifiability/assess_local_ID.jl
+++ src/ampk_models/identifiability/assess_local_ID.jl
@@ -2,20 +2,13 @@ using StructuralIdentifiability
 
 file_list = ["../odes/ampk_MA_double_mech.jl", "../odes/ampk_MA_single_mech.jl", "../odes/ampk_MM_double_mech.jl", "../odes/ampk_MM_single_mech.jl", "../odes/ampk_newmech_MA_single.jl", "../odes/ampk_qss_single.jl"]
 model_list = ["MA_double", "MA_single", "MM_double", "MM_single", "newmech_MA_single", "qss_single"]
-params_to_test = [
-    ["kOffAMP", "kOffADP", "kOffATP", "kOnCaMKK", "kPhosCaMKK", "kOnLKB1", "kPhosLKB1", "kOnPP", "kDephosPP", "kOnAMPK", "kPhosAMPK", "kOnPP1", "kDephosPP1"],
-    ["kOffAMP", "kOffADP", "kOffATP", "kOnCaMKK", "kPhosCaMKK", "kOnLKB1", "kPhosLKB1", "kOnPP", "kDephosPP", "kOnAMPK", "kPhosAMPK", "kOnPP1", "kDephosPP1"],
-    ["kOffAMP", "kOffADP", "kOffATP", "kCaMKK", "KmCaMKK", "kLKB1", "KmLKB1", "kPP", "KmPP", "kAMPK", "KmAMPK", "kPP1", "KmPP1"], 
-    ["kOffAMP", "kOffADP", "kOffATP", "kCaMKK", "KmCaMKK", "kLKB1", "KmLKB1", "kPP", "KmPP", "kAMPK", "KmAMPK", "kPP1", "KmPP1"], 
-    ["kOffAMP", "kOffADP", "kOffATP", "kOnCaMKK", "kPhosCaMKK", "kOnLKB1", "kPhosLKB1", "kOnPP", "kDephosPP", "kOnAMPK", "kPhosAMPK", "kOnPP1", "kDephosPP1", "beta"],
-    ["kOffAMP", "kOffADP", "kOffATP", "kCaMKK", "KmCaMKK", "kLKB1", "KmLKB1", "kPP", "KmPP", "kAMPK", "KmAMPK", "kPP1", "KmPP1", "beta"]
-]
 
-for (file, model, plist) in zip(file_list, model_list, params_to_test)
+
+for (file, model) in zip(file_list, model_list)
     println("Running: $model...")
     include(file)
 
-    local_id = assess_local_identifiability(ode, 0.99, plist)
+    local_id = assess_local_identifiability(ode, 0.99)
 
     # save to file for this model
     # create file to save results
