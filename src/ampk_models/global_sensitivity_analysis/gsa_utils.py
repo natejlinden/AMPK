9b856ad699d1ca983c0003c7ce07a1cb8dec4f84
Nathaniel Linden <nlinden@ucsd.edu>
2023-06-05 19:40:39 -0700

    updated compute param funcs and added correct qoi to model eval func

--- src/ampk_models/global_sensitivity_analysis/gsa_utils.py
+++ src/ampk_models/global_sensitivity_analysis/gsa_utils.py
@@ -34,7 +34,7 @@ def solve_to_steady_state(params, rhs, y0, event_rtol=1e-12, event_atol=1e-12):
 # jitable function to compute the qois
 @jax.jit
 def single_model_eval(params, rhs_basal, rhs_stress, y0, ampkar_idx, 
-                      pampkar_idx, event_rtol, event_atol):
+                      ampkar_idxs, pampkar_idxs, event_rtol, event_atol):
     # update y0 for AMPKAR
     y0 = y0.at[ampkar_idx].set(params[-1])
 
@@ -46,21 +46,25 @@ def single_model_eval(params, rhs_basal, rhs_stress, y0, ampkar_idx,
 
     # use ratio of pAMPKAR/AMPKARtot
     # note that params[-1] is the IC of AMPKAR, which the equal to AMPKARtot
-    basal = sol_basal.ys[0,pampkar_idx]/params[-1]
-    stress = sol_stress.ys[0,pampkar_idx]/params[-1]
-    change = stress - basal
-    norm_change = change / basal
+    # basal = sol_basal.ys[0,pampkar_idx]/params[-1]
+    # stress = sol_stress.ys[0,pampkar_idx]/params[-1]
+    # change = stress - basal
+    # norm_change = change / basal
+    AMPKAR_tot_basal = np.sum(sol_basal.ys[:,ampkar_idxs], axis=1)
+    pAMPKAR_basal = np.sum(sol_basal.ys[:,pampkar_idxs], axis=1)
+    pAMPKAR_stress = np.sum(sol_stress.ys[:,pampkar_idxs], axis=1)
+    qoi = (pAMPKAR_stress/AMPKAR_tot_basal)-(pAMPKAR_basal[-1]/AMPKAR_tot_basal[-1])
     
-    return jnp.array([norm_change, change, stress, sol_basal.ts[0], sol_stress.ts[0]])
+    return jnp.array([qoi, sol_basal.ts[0], sol_stress.ts[0]])
 
 @jax.jit
 def single_model_eval_nansafe(params, rhs_basal, rhs_stress, y0, ampkar_idx, 
-                              pampkar_idx, event_rtol=1e-12, event_atol=1e-12):
+                              pampkar_idx, qoi_func, event_rtol=1e-12, event_atol=1e-12):
     pred = jnp.sum(jnp.isnan(params))
     false_fun = lambda params: single_model_eval(params, rhs_basal, rhs_stress, 
                                                  y0, ampkar_idx, pampkar_idx, 
                                                  event_rtol=1e-12, event_atol=1e-12)
-    true_fun = lambda params: jnp.array([jnp.nan, jnp.nan, jnp.nan, jnp.nan, jnp.nan])
+    true_fun = lambda params: jnp.array([jnp.nan, jnp.nan, jnp.nan])
     return lax.cond(pred, true_fun, false_fun, params)
 
 # function to compute all MA params from sampled params
@@ -71,45 +75,44 @@ def compute_MA_params(params):
             params[1], # kOffADP
             1.0, # kOnATP
             params[2], # kOffATP
-            params[3], # kOnCaMKK
-            1.0, # kOffCaMKK
+            1.0, # kOnCaMKK
+            params[3], # kOffCaMKK
             params[4], # kPhosCaMKK
-            params[5], # kOnLKB1
-            1.0, # kOffLKB1
+            1.0, # kOnLKB1
+            params[5], # kOffLKB1
             params[6], # kPhosLKB1
-            params[7], # kOnPP
-            1.0, # kOffPP
+            1.0, # kOnPP
+            params[7], # kOffPP
             params[8], # kDephosPP
-            params[9], # kOnAMPK
-            1.0, # kOffAMPK
+            1.0, # kOnAMPK
+            params[9], # kOffAMPK
             params[10], # kPhosAMPK
-            params[11], # kOnPP1
-            1.0, # kOffPP1
-            params[12],
-            params[13]) # kDephosPP1
+            1.0, # kOnPP1
+            params[11], # kOffPP1
+            params[12], # kDephosPP1
+            params[13]) # AMPKAR_0
 
 def compute_newmech_MA_params(params):
     return (1.0, # kOnAMP
             params[0], # kOffAMP
-            params[1], # kOnCaMKK
-            1.0, # kOffCaMKK
+            1.0, # kOnCaMKK
+            params[1], # kOffCaMKK
             params[2], # kPhosCaMKK
-            params[3], # kOnLKB1
-            1.0, # kOffLKB1
+            1.0, # kOnLKB1
+            params[3], # kOffLKB1
             params[4], # kPhosLKB1
-            params[5], # kOnPP
-            1.0, # kOffPP
+            1.0, # kOnPP
+            params[5], # kOffPP
             params[6], # kDephosPP
-            params[7], # kOnAMPK
-            1.0, # kOffAMPK
+            1.0, # kOnAMPK
+            params[7], # kOffAMPK
             params[8], # kPhosAMPK
-            params[9], # kOnPP1
-            1.0, # kOffPP1
-            params[10], # kDephosPP1
-            1.0, # alpha
+            1.0, # kOnPP1
+            params[9], # kOffPP1
+            params[10], #kDephosPP1 
+            1.0, #alpha
             params[11], # beta
-            params[12], # AMPKAR_0 = AMPKARtot
-            )
+            params[12]) # AMPKAR_0
 
 def mass_action_to_michaelis_menten(k_rev, k_for, k_cat, Et):
 	"""function to convert from mass action to michaelis menten parameters
