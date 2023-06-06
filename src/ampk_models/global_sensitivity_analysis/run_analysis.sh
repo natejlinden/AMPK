9f2b320af2705bef9e3af49b8f42414eae4ba3a0
Nathaniel Linden <nlinden@ucsd.edu>
2023-06-06 11:29:29 -0700

    script to run analsysis for all files

--- src/ampk_models/global_sensitivity_analysis/run_analysis.sh
+++ src/ampk_models/global_sensitivity_analysis/run_analysis.sh
@@ -1,8 +1,36 @@
-echo "Running GSA analysis"
-python analyze_GSA.py
+echo "Running MA_double"
+python analyze_GSA.py MA_double_mech nominal_params_MA.csv param_bounds_MA.csv ../../../figures/
+python plot_trajectories.py ./ MA_double_mech ampk_MA_double_mech_diffrax \
+    MA_double.json nominal_params_MA.csv param_bounds_MA.csv \
+    compute_MA_params 100 ../../../figures/
+echo "done"
 
-echo "plotting trajectories"
-python MA_double_mech_trajectories.py
-#python newmech_MA_single_trajectories.py
+echo "Running MA_single"
+python analyze_GSA.py MA_single_mech nominal_params_MA.csv param_bounds_MA.csv ../../../figures/
+python plot_trajectories.py ./ MA_single_mech ampk_MA_single_mech_diffrax \
+    MA_double.json nominal_params_MA.csv param_bounds_MA.csv \
+    compute_MA_params 100 ../../../figures/
+echo "done"
 
-echo "Done"
+# echo "Running MM_double"
+# python analyze_GSA.py MM_double_mech nominal_params_MM.csv param_bounds_MM.csv ../../../figures/
+# python plot_trajectories.py ./ MM_double_mech ampk_MM_double_mech_diffrax \
+#     MM_double.json nominal_params_MM.csv param_bounds_MM.csv \
+#     compute_MM_params 100 ../../../figures/
+# echo "done"
+
+# echo "Running MM_single"
+# python analyze_GSA.py MM_single_mech nominal_params_MM.csv param_bounds_MM.csv ../../../figures/
+# python plot_trajectories.py ./ MM_single_mech ampk_MM_single_mech_diffrax \
+#     MM_double.json nominal_params_MM.csv param_bounds_MM.csv \
+#     compute_MM_params 100 ../../../figures/
+# echo "done"
+
+# echo "Running newmech_MA_single"
+# python analyze_GSA.py newmech_MA_single nominal_params_newmech_MA.csv param_bounds_newmech_MA.csv ../../../figures/
+# python plot_trajectories.py ./ ampk_newmech_MA_single_diffrax \
+#     newmech_MA_single.json nominal_params_newmech_MA.csv param_bounds_newmech_MA.csv \
+#     compute_newmech_MA_params 100 ../../../figures/
+# echo "done"
+
+echo "Analysis complete"
