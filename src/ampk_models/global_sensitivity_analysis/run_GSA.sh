e75c5576fcde6cf65035990fc99b269b2534ab35
Nate <natejlinden@gmail.com>
2023-06-07 09:24:59 -0700

    changes

--- src/ampk_models/global_sensitivity_analysis/run_GSA.sh
+++ src/ampk_models/global_sensitivity_analysis/run_GSA.sh
@@ -1,24 +1,24 @@
-echo "Sampling MA_double"
-python GSA_sampling.py 0.5 ./ MA_double_mech ampk_MA_double_mech_diffrax \
-    MA_double.json nominal_params_MA.csv param_bounds_MA.csv \
-    compute_MA_params
+#echo "Sampling MA_double"
+#python GSA_sampling.py 0.5 ./ MA_double_mech ampk_MA_double_mech_diffrax \
+#    MA_double.json nominal_params_MA.csv param_bounds_MA.csv \
+#    compute_MA_params
 
-echo "Sampling MA_single"
-python GSA_sampling.py 0.5 ./ MA_single_mech ampk_MA_single_mech_diffrax \
-    MA_single.json nominal_params_MA.csv param_bounds_MA.csv \
-    compute_MA_params
+#echo "Sampling MA_single"
+#python GSA_sampling.py 0.5 ./ MA_single_mech ampk_MA_single_mech_diffrax \
+#    MA_single.json nominal_params_MA.csv param_bounds_MA.csv \
+#    compute_MA_params
 
-echo "Sampling MM_double"
-python GSA_sampling.py 0.5 ./ MM_double_mech ampk_MM_double_mech_diffrax \
-    MM_double.json nominal_params_MM.csv param_bounds_MM.csv \
-    compute_MM_params
+#echo "Sampling MM_double"
+#python GSA_sampling.py 0.5 ./ MM_double_mech ampk_MM_double_mech_diffrax \
+#    MM_double.json nominal_params_MM.csv param_bounds_MM.csv \
+#    compute_MM_params
 
 echo "Sampling MM_single"
 python GSA_sampling.py 0.5 ./ MM_single_mech ampk_MM_single_mech_diffrax \
     MM_single.json nominal_params_MM.csv param_bounds_MM.csv \
     compute_MM_params
 
-echo "Sampling newmech_MA_single"
-python GSA_sampling.py 0.5 ./ newmech_MA_single ampk_newmech_MA_single_diffrax \
-    newmech_MA_single.json nominal_params_newmech_MA.csv param_bounds_newmech_MA.csv \
-    compute_newmech_MA_params
+#echo "Sampling newmech_MA_single"
+#python GSA_sampling.py 0.5 ./ newmech_MA_single ampk_newmech_MA_single_diffrax \
+#    newmech_MA_single.json nominal_params_newmech_MA.csv param_bounds_newmech_MA.csv \
+#    compute_newmech_MA_params
--- src/ampk_models/global_sensitivity_analysis/run_analysis.sh
+++ src/ampk_models/global_sensitivity_analysis/run_analysis.sh
@@ -1,14 +1,14 @@
-echo "Running MA_double"
-python analyze_GSA.py MA_double_mech nominal_params_MA.csv param_bounds_MA.csv ../../../figures/
-python plot_trajectories.py ./ MA_double_mech ampk_MA_double_mech_diffrax \
-    MA_double.json compute_MA_params 100 ../../../figures/
-echo "done"
+#echo "Running MA_double"
+#python analyze_GSA.py MA_double_mech nominal_params_MA.csv param_bounds_MA.csv ../../../figures/
+#python plot_trajectories.py ./ MA_double_mech ampk_MA_double_mech_diffrax \
+#    MA_double.json compute_MA_params 100 ../../../figures/
+#echo "done"
 
-echo "Running MA_single"
-python analyze_GSA.py MA_single_mech nominal_params_MA.csv param_bounds_MA.csv ../../../figures/
-python plot_trajectories.py ./ MA_single_mech ampk_MA_single_mech_diffrax \
-    MA_single.json compute_MA_params 100 ../../../figures/
-echo "done"
+#echo "Running MA_single"
+#python analyze_GSA.py MA_single_mech nominal_params_MA.csv param_bounds_MA.csv ../../../figures/
+#python plot_trajectories.py ./ MA_single_mech ampk_MA_single_mech_diffrax \
+#    MA_single.json compute_MA_params 100 ../../../figures/
+#echo "done"
 
 echo "Running MM_double"
 python analyze_GSA.py MM_double_mech nominal_params_MM.csv param_bounds_MM.csv ../../../figures/
@@ -24,7 +24,7 @@ echo "done"
 
 echo "Running newmech_MA_single"
 python analyze_GSA.py newmech_MA_single nominal_params_newmech_MA.csv param_bounds_newmech_MA.csv ../../../figures/
-python plot_trajectories.py ./ ampk_newmech_MA_single_diffrax \
+python plot_trajectories.py ./ newmech_MA_single ampk_newmech_MA_single_diffrax \
     newmech_MA_single.json compute_newmech_MA_params 100 ../../../figures/
 echo "done"
 
