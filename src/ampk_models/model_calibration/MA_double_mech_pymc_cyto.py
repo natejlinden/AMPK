9a77dd8faee27e02c64ccd8e02bbb0466aa0eac1
Nathaniel Linden <nlinden@ucsd.edu>
2023-06-08 18:49:33 -0700

    change smapling

--- src/ampk_models/model_calibration/MA_double_mech_pymc_cyto.py
+++ src/ampk_models/model_calibration/MA_double_mech_pymc_cyto.py
@@ -298,8 +298,8 @@ with cyto_model:
 with cyto_model:
     # draw 4000 posterior samples
     # numpyro NUTS
-    # idata = pm.sample(draws=4000, chains=4, idata_kwargs={'log_likelihood':True})
-    idata = pmsj.sample_numpyro_nuts(draws=4000, chains=4, idata_kwargs={'log_likelihood':True})
+    idata = pm.sample(draws=4000, chains=1, idata_kwargs={'log_likelihood':True})
+    # idata = pmsj.sample_numpyro_nuts(draws=4000, chains=4, idata_kwargs={'log_likelihood':True})
 az.to_netcdf(idata, dir + base_name + '/cyto_posterior.nc')
 
 # posterior predictive samples
