f0790014da915a9f048256618fad4cee0a09636d
Nate Linden-Santangeli <nlinden@ucsd.edu>
2024-08-10 20:23:56 -0700

    increase nsamples

--- src/ampk_models/global_sensitivity_analysis/GSA.sh
+++ src/ampk_models/global_sensitivity_analysis/GSA.sh
@@ -1,2 +1,2 @@
 # MA Double
-python GSA_sampling.py -model MA_double_mech -free_params kOffAMP,kOffADP,kOffATP,kOffCaMKK,kPhosCaMKK,kOffLKB1,kPhosLKB1,kOffPP,kDephosPP,kOffAMPK,kPhosAMPK,kOffPP1,kDephosPP1 -model_info_file MA_double.json -upper_mult 1e2 -lower_mult 1e-2 -metab_params_file metabolism_params.json -nsamples 4 -gsa_method sobol
+python GSA_sampling.py -model MA_double_mech -free_params kOffAMP,kOffADP,kOffATP,kOffCaMKK,kPhosCaMKK,kOffLKB1,kPhosLKB1,kOffPP,kDephosPP,kOffAMPK,kPhosAMPK,kOffPP1,kDephosPP1 -model_info_file ../odes/MA_double.json -upper_mult 1e2 -lower_mult 1e-2 -metab_params_file ../odes/metabolism_params.json -nsamples 512 -gsa_method sobol -savedir /tscc/lustre/ddn/scratch/nlinden/ampk 
