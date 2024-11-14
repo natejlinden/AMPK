eb898de92424527392a57c6836acb486c75da406
Nathaniel Linden <nlinden@ucsd.edu>
2024-11-13 16:22:14 -0800

    script to run GSA sampling for all models

--- src/ampk_models/global_sensitivity_analysis/GSA.sh
+++ src/ampk_models/global_sensitivity_analysis/GSA.sh
@@ -1,15 +1,14 @@
-# MA Double
-python GSA_sampling.py -model MA_double_mech -free_params kOffAMP,kOffADP,kOffATP,kOffCaMKK,kPhosCaMKK,kOffLKB1,kPhosLKB1,kOffPP,kDephosPP,kOffAMPK,kPhosAMPK,kOffPP1,kDephosPP1 -model_info_file ../odes/MA_double.json -upper_mult 1e2 -lower_mult 1e-2 -metab_params_file ../odes/metabolism_params.json -nsamples 2048 -gsa_method sobol -savedir /tscc/lustre/ddn/scratch/nlinden/ampk -tmax 5e5
+# Coccimiglio
+python GSA_sampling.py -model ampk_Coccimiglio -free_params k6r,k7r,k8r,k9r,k10r,k11r,Km12,Km13,Km14,Km15,Km16,Km17,Km18,Km19,Vmaxkinase,VmaxkinaseATP,VmaxkinaseADP,VmaxkinaseAMP,Vmaxppase,VmaxppaseATP,VmaxppaseADP,VmaxppaseAMP,Km_pAMPK,k_pAMPK,Km_AMP_pAMPK,k_AMP_pAMPK,Km_ADP_pAMPK,k_ADP_pAMPK,Km_ATP_pAMPK,k_ATP_pAMPK -model_info_file ../odes/ampk_Coccimiglio.json -upper_mult 1e2 -lower_mult 1e-2 -metab_params_file ../odes/metabolism_params_Coccimiglio.json -nsamples 2048 -gsa_method sobol -savedir ../../../results/GSA/ -tmax 5e5
 
 # MA Single
-python GSA_sampling.py -model MA_single_mech -free_params kOffAMP,kOffADP,kOffATP,kOffCaMKK,kPhosCaMKK,kOffLKB1,kPhosLKB1,kOffPP,kDephosPP,kOffAMPK,kPhosAMPK,kOffPP1,kDephosPP1 -model_info_file ../odes/MA_single.json -upper_mult 1e2 -lower_mult 1e-2 -metab_params_file ../odes/metabolism_params.json -nsamples 2048 -gsa_method sobol -savedir /tscc/lustre/ddn/scratch/nlinden/ampk -tmax 5e5
-
-# MM Double
-python GSA_sampling.py -model MM_double_mech -free_params kOffAMP,kOffADP,kOffATP,kPhosCaMKK,KmCaMKK,kPhosLKB1,KmLKB1,kDephosPP,KmPP -model_info_file ../odes/MA_double.json -upper_mult 1e2 -lower_mult 1e-2 -metab_params_file ../odes/metabolism_params.json -nsamples 2048 -gsa_method sobol -savedir /tscc/lustre/ddn/scratch/nlinden/ampk -tmax 5e5
+python GSA_sampling.py -model MA_single -free_params kOffAMP,kOffADP,kOffATP,kOffCaMKK,kPhosCaMKK,kOffLKB1,kPhosLKB1,kOffPP,kDephosPP,kOffAMPK,kPhosAMPK,kOffPP1,kDephosPP1 -model_info_file ../odes/MA_single.json -upper_mult 1e2 -lower_mult 1e-2 -metab_params_file ../odes/metabolism_params_Coccimiglio.json -nsamples 2048 -gsa_method sobol -savedir ../../../results/GSA/ -tmax 5e5
 
 # MM Single
-python GSA_sampling.py -model MM_single_mech -free_params kOffAMP,kOffADP,kOffATP,kPhosCaMKK,KmCaMKK,kPhosLKB1,KmLKB1,kDephosPP,KmPP -model_info_file ../odes/MA_single.json -upper_mult 1e2 -lower_mult 1e-2 -metab_params_file ../odes/metabolism_params.json -nsamples 2048 -gsa_method sobol -savedir /tscc/lustre/ddn/scratch/nlinden/ampk -tmax 5e5
+python GSA_sampling.py -model MM_single -free_params kOffAMP,kOffADP,kOffATP,kCaMKK,KmCaMKK,kLKB1,KmLKB1,kPP,KmPP -model_info_file ../odes/MM_single.json -upper_mult 1e2 -lower_mult 1e-2 -metab_params_file ../odes/metabolism_params_Coccimiglio.json -nsamples 2048 -gsa_method sobol -savedir ../../../results/GSA/ -tmax 5e5
 
-# newmech MA single
-python GSA_sampling.py -model newmech_MA_single -free_params kOffAMP,kOffADP,kOffATP,kOffCaMKK,kPhosCaMKK,kOffLKB1,kPhosLKB1,kOffPP,kDephosPP,kOffAMPK,kPhosAMPK,kOffPP1,kDephosPP1 -model_info_file ../odes/newmech_MA_single.json -upper_mult 1e2 -lower_mult 1e-2 -metab_params_file ../odes/metabolism_params.json -nsamples 2048 -gsa_method sobol -savedir /tscc/lustre/ddn/scratch/nlinden/ampk -tmax 5e5
+# MA Nonessential
+python GSA_sampling.py -model MA_nonessential -free_params kOffAMP,kOffADP,kOffATP,kOffCaMKK,kPhosCaMKK,kOffLKB1,kPhosLKB1,kOffPP,kDephosPP,kOffAMPK,kPhosAMPK,kOffPP1,kDephosPP1,alphaLKB1,alphaPP,betaAMP -model_info_file ../odes/MA_nonessential.json -upper_mult 1e2 -lower_mult 1e-2 -special_bounds ./special_bounds.json -metab_params_file ../odes/metabolism_params_Coccimiglio.json -nsamples 2048 -gsa_method sobol -savedir ../../../results/GSA/ -tmax 5e5
 
+# MM Nonessential
+python GSA_sampling.py -model MM_nonessential -free_params kOffAMP,kOffADP,kOffATP,kPhosCaMKK,KmCaMKK,kPhosLKB1,KmLKB1,kDephosPP,KmPP,alphaLKB1,alphaPP,betaAMP -model_info_file ../odes/MM_nonessential.json -upper_mult 1e2 -lower_mult 1e-2 -special_bounds ./special_bounds.json -metab_params_file ../odes/metabolism_params_Coccimiglio.json -nsamples 2048 -gsa_method sobol -savedir ../../../results/GSA/ -tmax 5e5
