8c954fe8bdec311b6abe44dac95b91a6763c3437
Nathaniel Linden <nlinden@ucsd.edu>
2024-12-02 14:01:32 -0800

    increase number of warmup samples

--- src/mass-action_vs_michaelis-menten/inferenc_run.sh
+++ src/mass-action_vs_michaelis-menten/inferenc_run.sh
@@ -1,8 +1,8 @@
 # MA - Duggleby and Clarke - Pyruvate kinase data
-XLA_FLAGS="--xla_force_host_platform_device_count=6" python inference.py -model MA -free_params k_f,k_r,k_cat -data_file ./data/Duggleby_Clarke_1991_Fig2.csv -model_info_file ./MA_pyruvate_kinase.json -savedir ./results/ -nwarmup 5000 -nsamples 6000 -nchains 6 -rtol 1e-7 -atol 1e-7 -pcoeff 0.3 -icoeff 0.4 -sampler AIES
+XLA_FLAGS="--xla_force_host_platform_device_count=6" python inference.py -model MA -free_params k_f,k_r,k_cat -data_file ./data/Duggleby_Clarke_1991_Fig2.csv -model_info_file ./MA_pyruvate_kinase.json -savedir ../../results/MA_v_MM/pyru_kin_ -nwarmup 10000 -nsamples 6000 -nchains 12 -rtol 1e-7 -atol 1e-7 -pcoeff 0.3 -icoeff 0.4 -sampler AIES
 
 # MM - Duggleby and Clarke - Pyruvate kinase data
-XLA_FLAGS="--xla_force_host_platform_device_count=6" python inference.py -model MM -free_params V_max,K_m -data_file ./data/Duggleby_Clarke_1991_Fig2.csv -model_info_file ./MM_pyruvate_kinase.json -savedir ./results/ -nwarmup 5000 -nsamples 6000 -nchains 6 -rtol 1e-7 -atol 1e-7 -pcoeff 0.3 -icoeff 0.4 -sampler AIES
+XLA_FLAGS="--xla_force_host_platform_device_count=6" python inference.py -model MM -free_params V_max,K_m -data_file ./data/Duggleby_Clarke_1991_Fig2.csv -model_info_file ./MM_pyruvate_kinase.json -savedir ../../results/MA_v_MM/pyru_kin_ -nwarmup 10000 -nsamples 6000 -nchains 12 -rtol 1e-7 -atol 1e-7 -pcoeff 0.3 -icoeff 0.4 -sampler AIES
 
 # Hill - Duggleby and Clarke - Pyruvate kinase data
-XLA_FLAGS="--xla_force_host_platform_device_count=6" python inference.py -model Hill -free_params V_max,K_m,n -data_file ./data/Duggleby_Clarke_1991_Fig2.csv -model_info_file ./Hill_pyruvate_kinase.json -savedir ./results/ -nwarmup 5000 -nsamples 6000 -nchains 6 -rtol 1e-7 -atol 1e-7 -pcoeff 0.3 -icoeff 0.4 -sampler AIES
+XLA_FLAGS="--xla_force_host_platform_device_count=6" python inference.py -model Hill -free_params V_max,K_m,n -data_file ./data/Duggleby_Clarke_1991_Fig2.csv -model_info_file ./Hill_pyruvate_kinase.json -savedir ../../results/MA_v_MM/pyru_kin_ -nwarmup 10000 -nsamples 6000 -nchains 12 -rtol 1e-7 -atol 1e-7 -pcoeff 0.3 -icoeff 0.4 -sampler AIES
