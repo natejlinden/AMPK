d63761415830162be059bc244731e055b5f7dd63
Nathaniel Linden <nlinden@ucsd.edu>
2024-12-17 11:54:18 -0800

    clip negative concentrations to min of 0 to avoid Nan/Inf in sol which causes issues when computing gradients

--- src/mass-action_vs_michaelis-menten/Hill_diffrax.py
+++ src/mass-action_vs_michaelis-menten/Hill_diffrax.py
@@ -12,6 +12,8 @@ class Hill(eqx.Module):
 
     def __call__(self, t, y, args):
 
+        y = eqx.error_if(y, jnp.any(jnp.isnan(y) | jnp.isinf(y)), "y is NaN or Inf")
+
         # unpack parameters
         V_max   = args[0]
         K_m     = args[1]
@@ -20,13 +22,30 @@ class Hill(eqx.Module):
         # unpack states
         S = y[0]
         P = y[1]
+
+        # clip S if its below 0!
+        # For some reason this generates negative values when the time step is not ridiculously small
+        # so we need to clip it to 0 to avoid numerical trouble when computing gradients
+        S = jnp.clip(S, min=0, max=None)
+        # P = jnp.clip(P, min=0, max=None)
     
         # FLUXES
         # single AXP complexing
         J1 = V_max*(S**n)/(K_m + (S**n))
+
+        # clip if its too big
+        # J1 = jnp.clip(J1, max=1e6, min=None)
         
         # now return the odes for each state variable
         d_S = -J1
         d_P = J1
 
-        return jnp.array([d_S, d_P])
+        dydt = jnp.array([d_S, d_P])
+
+        # jax.debug.print(
+        #     "t: {t}, x: {x}, dxdt: {dxdt}, args: {args}", t=t, x=y, dxdt=dydt, args=args, ordered=True
+        # )
+
+        dydt = eqx.error_if(dydt, jnp.any(jnp.isnan(dydt) | jnp.isinf(dydt)), "dydt is NaN or Inf")
+
+        return dydt
