cdc8b75c24c93ee42955d5924ebbca90fad7b917
Nathaniel Linden <nlinden@ucsd.edu>
2025-01-16 09:05:57 -0800

    comments and added a Pytensor Op for ODE solution w/o a pytensor gradient Op. Useful when using Jaxc-based samplers that will just take grads w/ Jax autodiff directly

--- src/ampk_models/pymc_jax_ode.py
+++ src/ampk_models/pymc_jax_ode.py
@@ -4,6 +4,8 @@ import pytensor.tensor as pt
 from pytensor.graph import Apply, Op
 
 class SolOp(Op):
+    """ Pytensor Op for the solution of an ODE system 
+    using Diffrax with an associated pytensor gradient Op. See VJPSOLOp below. """
     def __init__(self, sol_op_jax_jitted, vjp_sol_op):
         self.sol_op_jax_jitted = sol_op_jax_jitted
         self.vjp_sol_op = vjp_sol_op
@@ -24,6 +26,8 @@ class SolOp(Op):
         return self.vjp_sol_op(inputs, gz)
     
 class VJPSolOp(Op):
+    """ Pytensor Op for the gradient of the solution of an ODE system 
+    using Diffrax """
     def __init__(self, vjp_sol_op_jax_jitted):
         self.vjp_sol_op_jax_jitted = vjp_sol_op_jax_jitted
 
@@ -38,3 +42,24 @@ class VJPSolOp(Op):
         result = self.vjp_sol_op_jax_jitted(gz, *params)
         for i, res in enumerate(result):
             outputs[i][0] = np.asarray(res, dtype="float64")
+
+
+class SolOp_noGrad(Op):
+    """ Pytensor Op for the solution of the ODE system using Diffrax w/o a pytensor gradient Op. """
+    def __init__(self, sol_op_jax_jitted, vjp_sol_op):
+        self.sol_op_jax_jitted = sol_op_jax_jitted
+
+    def make_node(self, *inputs):
+        # Convert our inputs to symbolic variables
+        inputs = [pt.as_tensor_variable(inp) for inp in inputs]
+        # Assume the output to always be a float64 matrix
+        outputs = [pt.matrix()]
+        return Apply(self, inputs, outputs)
+
+    def perform(self, node, inputs, outputs):
+        result = self.sol_op_jax_jitted(*inputs)
+        outputs[0][0] = np.asarray(result, dtype="float64")
+        
+    def grad(self, inputs, output_grads):
+        def grad(self, inputs, output_grads):
+        raise NotImplementedError("PyTensor gradient of Op not implemented")
