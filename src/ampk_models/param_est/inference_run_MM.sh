7990f84e8919ceab7c72e05c30f09712e78efa32
Nathaniel Linden <nlinden@ucsd.edu>
2025-02-04 11:15:40 -0800

    update inference scripts

--- src/ampk_models/param_est/inference_run_MM.sh
+++ src/ampk_models/param_est/inference_run_MM.sh
@@ -3,21 +3,44 @@
 
 # MM single, cytosol
 # NUTS with default init
-XLA_FLAGS="--xla_force_host_platform_device_count=1" python inference_pymc.py -model MM_single -compartment cyto -free_params kOffAMP,kOffADP,kOffATP,kLKB1,kCaMKK -data_file ../../../Schmitt_et_al_2022_data/fig_2e_cyto.npz -model_info_file ../models/MM_single.json -metab_params_file ../models/metabolism_params_Coccimiglio.json -savedir ../../../results/param_est/ -nwarmup 3000 -nsamples 3000 -nchains 4 -sampler NUTS -lower_mult 1e-2 -upper_mult 1e4 -pcoeff 0.3 -icoeff 0.4 -rtol 1e-6 -atol 1e-6 --sample_posterior
+python inference_pymc.py -model MM_single -compartment cyto -free_params kOffAMP,kOffADP,kOffATP -data_file ../../../Schmitt_et_al_2022_data/fig_2e_cyto.npz -model_info_file ../models/MM_single.json -metab_params_file ../models/metabolism_params_Coccimiglio.json -savedir ../../../results/param_est/ -nwarmup 300 -nsamples 1000 -nchains 4 -sampler Nutpie -pcoeff 0.3 -icoeff 0.4 -rtol 1e-6 -atol 1e-6 --sample_posterior
 
 # ADVI with mean field approximation
-XLA_FLAGS="--xla_force_host_platform_device_count=1" python inference_pymc.py -model MM_single -compartment cyto -free_params kOffAMP,kOffADP,kOffATP,kLKB1,kCaMKK -data_file ../../../Schmitt_et_al_2022_data/fig_2e_cyto.npz -model_info_file ../models/MM_single.json -metab_params_file ../models/metabolism_params_Coccimiglio.json -savedir ../../../results/param_est/ -nsamples 4000 -nchains 4 -sampler ADVI -lower_mult 1e-2 -upper_mult 1e4 -pcoeff 0.3 -icoeff 0.4 -rtol 1e-6 -atol 1e-6 -n_advi_iter 60000 --sample_posterior
+python inference_pymc.py -model MM_single -compartment cyto -free_params kOffAMP,kOffADP,kOffATP -data_file ../../../Schmitt_et_al_2022_data/fig_2e_cyto.npz -model_info_file ../models/MM_single.json -metab_params_file ../models/metabolism_params_Coccimiglio.json -savedir ../../../results/param_est/ -nsamples 4000 -nchains 4 -sampler ADVI -pcoeff 0.3 -icoeff 0.4 -rtol 1e-6 -atol 1e-6 -n_advi_iter 150000 --sample_posterior
 
 # MM single, lysosome
 # NUTS with default init
-XLA_FLAGS="--xla_force_host_platform_device_count=1" python inference_pymc.py -model MM_single -compartment lyso -free_params kOffAMP,kOffADP,kOffATP,kLKB1,kCaMKK -data_file ../../../Schmitt_et_al_2022_data/fig_2b_lyso.npz -model_info_file ../models/MM_single.json -metab_params_file ../models/metabolism_params_Coccimiglio.json -savedir ../../../results/param_est/ -nwarmup 3000 -nsamples 3000 -nchains 4 -sampler NUTS -lower_mult 1e-2 -upper_mult 1e4 -pcoeff 0.3 -icoeff 0.4 -rtol 1e-6 -atol 1e-6 --sample_posterior
+python inference_pymc.py -model MM_single -compartment lyso -free_params kOffAMP,kOffADP,kOffATP -data_file ../../../Schmitt_et_al_2022_data/fig_2b_lyso.npz -model_info_file ../models/MM_single.json -metab_params_file ../models/metabolism_params_Coccimiglio.json -savedir ../../../results/param_est/ -nwarmup 300 -nsamples 1000 -nchains 4 -sampler Nutpie -pcoeff 0.3 -icoeff 0.4 -rtol 1e-6 -atol 1e-6 --sample_posterior
 
 # ADVI with mean field approximation
-XLA_FLAGS="--xla_force_host_platform_device_count=1" python inference_pymc.py -model MM_single -compartment lyso -free_params kOffAMP,kOffADP,kOffATP,kLKB1,kCaMKK -data_file ../../../Schmitt_et_al_2022_data/fig_2b_lyso.npz -model_info_file ../models/MM_single.json -metab_params_file ../models/metabolism_params_Coccimiglio.json -savedir ../../../results/param_est/ -nsamples 4000 -nchains 4 -sampler ADVI -lower_mult 1e-2 -upper_mult 1e4 -pcoeff 0.3 -icoeff 0.4 -rtol 1e-6 -atol 1e-6 -n_advi_iter 60000 --sample_posterior
+python inference_pymc.py -model MM_single -compartment lyso -free_params kOffAMP,kOffADP,kOffATP -data_file ../../../Schmitt_et_al_2022_data/fig_2b_lyso.npz -model_info_file ../models/MM_single.json -metab_params_file ../models/metabolism_params_Coccimiglio.json -savedir ../../../results/param_est/ -nsamples 4000 -nchains 4 -sampler ADVI -pcoeff 0.3 -icoeff 0.4 -rtol 1e-6 -atol 1e-6 -n_advi_iter 150000 --sample_posterior
 
 # MM single, mitochondria
 # NUTS with default init
-XLA_FLAGS="--xla_force_host_platform_device_count=1" python inference_pymc.py -model MM_single -compartment mito -free_params kOffAMP,kOffADP,kOffATP,kLKB1,kCaMKK -data_file ../../../Schmitt_et_al_2022_data/fig_2c_mito.npz -model_info_file ../models/MM_single.json -metab_params_file ../models/metabolism_params_Coccimiglio.json -savedir ../../../results/param_est/ -nwarmup 3000 -nsamples 3000 -nchains 4 -sampler NUTS -lower_mult 1e-2 -upper_mult 1e4 -pcoeff 0.3 -icoeff 0.4 -rtol 1e-6 -atol 1e-6 --sample_posterior
+python inference_pymc.py -model MM_single -compartment mito -free_params kOffAMP,kOffADP,kOffATP -data_file ../../../Schmitt_et_al_2022_data/fig_2c_mito.npz -model_info_file ../models/MM_single.json -metab_params_file ../models/metabolism_params_Coccimiglio.json -savedir ../../../results/param_est/ -nwarmup 300 -nsamples 1000 -nchains 4 -sampler Nutpie -pcoeff 0.3 -icoeff 0.4 -rtol 1e-6 -atol 1e-6 --sample_posterior
 
 # ADVI with mean field approximation
-XLA_FLAGS="--xla_force_host_platform_device_count=1" python inference_pymc.py -model MM_single -compartment mito -free_params kOffAMP,kOffADP,kOffATP,kLKB1,kCaMKK -data_file ../../../Schmitt_et_al_2022_data/fig_2c_mito.npz -model_info_file ../models/MM_single.json -metab_params_file ../models/metabolism_params_Coccimiglio.json -savedir ../../../results/param_est/ -nsamples 4000 -nchains 4 -sampler ADVI -lower_mult 1e-2 -upper_mult 1e4 -pcoeff 0.3 -icoeff 0.4 -rtol 1e-6 -atol 1e-6 -n_advi_iter 60000 --sample_posterior
+python inference_pymc.py -model MM_single -compartment mito -free_params kOffAMP,kOffADP,kOffATP -data_file ../../../Schmitt_et_al_2022_data/fig_2c_mito.npz -model_info_file ../models/MM_single.json -metab_params_file ../models/metabolism_params_Coccimiglio.json -savedir ../../../results/param_est/ -nsamples 4000 -nchains 4 -sampler ADVI -pcoeff 0.3 -icoeff 0.4 -rtol 1e-6 -atol 1e-6 -n_advi_iter 150000 --sample_posterior
+
+##################################################
+## GSA Uninformed -ie all parameters are free
+# MM single, cytosol
+# NUTS with default init
+python inference_pymc.py -model MM_single -compartment cyto -free_params kOffAMP,kOffADP,kOffATP,kCaMKK,KmCaMKK,kLKB1,KmLKB1,kPP,KmPP -data_file ../../../Schmitt_et_al_2022_data/fig_2e_cyto.npz -model_info_file ../models/MM_single.json -metab_params_file ../models/metabolism_params_Coccimiglio.json -savedir ../../../results/param_est/all_free/ -nwarmup 300 -nsamples 1000 -nchains 4 -sampler Nutpie -pcoeff 0.3 -icoeff 0.4 -rtol 1e-6 -atol 1e-6 --sample_posterior
+
+# ADVI with mean field approximation
+python inference_pymc.py -model MM_single -compartment cyto -free_params kOffAMP,kOffADP,kOffATP,kCaMKK,KmCaMKK,kLKB1,KmLKB1,kPP,KmPP -data_file ../../../Schmitt_et_al_2022_data/fig_2e_cyto.npz -model_info_file ../models/MM_single.json -metab_params_file ../models/metabolism_params_Coccimiglio.json -savedir ../../../results/param_est/all_free/ -nsamples 4000 -nchains 4 -sampler ADVI -pcoeff 0.3 -icoeff 0.4 -rtol 1e-6 -atol 1e-6 -n_advi_iter 150000 --sample_posterior
+
+# MM single, lysosome
+# NUTS with default init
+python inference_pymc.py -model MM_single -compartment lyso -free_params kOffAMP,kOffADP,kOffATP,kCaMKK,KmCaMKK,kLKB1,KmLKB1,kPP,KmPP -data_file ../../../Schmitt_et_al_2022_data/fig_2b_lyso.npz -model_info_file ../models/MM_single.json -metab_params_file ../models/metabolism_params_Coccimiglio.json -savedir ../../../results/param_est/all_free/ -nwarmup 300 -nsamples 1000 -nchains 4 -sampler Nutpie -pcoeff 0.3 -icoeff 0.4 -rtol 1e-6 -atol 1e-6 --sample_posterior
+
+# ADVI with mean field approximation
+python inference_pymc.py -model MM_single -compartment lyso -free_params kOffAMP,kOffADP,kOffATP,kCaMKK,KmCaMKK,kLKB1,KmLKB1,kPP,KmPP -data_file ../../../Schmitt_et_al_2022_data/fig_2b_lyso.npz -model_info_file ../models/MM_single.json -metab_params_file ../models/metabolism_params_Coccimiglio.json -savedir ../../../results/param_est/all_free/ -nsamples 4000 -nchains 4 -sampler ADVI -pcoeff 0.3 -icoeff 0.4 -rtol 1e-6 -atol 1e-6 -n_advi_iter 150000 --sample_posterior
+
+# MM single, mitochondria
+# NUTS with default init
+python inference_pymc.py -model MM_single -compartment mito -free_params kOffAMP,kOffADP,kOffATP,kCaMKK,KmCaMKK,kLKB1,KmLKB1,kPP,KmPP -data_file ../../../Schmitt_et_al_2022_data/fig_2c_mito.npz -model_info_file ../models/MM_single.json -metab_params_file ../models/metabolism_params_Coccimiglio.json -savedir ../../../results/param_est/all_free/ -nwarmup 300 -nsamples 1000 -nchains 4 -sampler Nutpie -pcoeff 0.3 -icoeff 0.4 -rtol 1e-6 -atol 1e-6 --sample_posterior
+
+# ADVI with mean field approximation
+python inference_pymc.py -model MM_single -compartment mito -free_params kOffAMP,kOffADP,kOffATP,kCaMKK,KmCaMKK,kLKB1,KmLKB1,kPP,KmPP -data_file ../../../Schmitt_et_al_2022_data/fig_2c_mito.npz -model_info_file ../models/MM_single.json -metab_params_file ../models/metabolism_params_Coccimiglio.json -savedir ../../../results/param_est/all_free/ -nsamples 4000 -nchains 4 -sampler ADVI -pcoeff 0.3 -icoeff 0.4 -rtol 1e-6 -atol 1e-6 -n_advi_iter 150000 --sample_posterior
