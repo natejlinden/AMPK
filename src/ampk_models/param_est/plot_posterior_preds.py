25818ba17f5ac46ab22f991ad28841693157162f
Nathaniel Linden <nlinden@ucsd.edu>
2025-02-10 11:22:02 -0800

    plot bar plots of thalf and max act for knockdown sims

--- src/ampk_models/param_est/plot_posterior_preds.py
+++ src/ampk_models/param_est/plot_posterior_preds.py
@@ -54,13 +54,6 @@ lyso_data, _, lyso_times = load_data('../../../Schmitt_et_al_2022_data/fig_2b_ly
 mito_data, _, mito_times = load_data('../../../Schmitt_et_al_2022_data/fig_2c_mito.npz', 
                                      to_seconds=False, constant_std=False)
 
-# dictionary to store idata
-idata_dict = {
-    'cyto':{model:{sampler:None for sampler in samplers} for model in models_free_params.keys()},
-    'lyso':{model:{sampler:None for sampler in samplers} for model in models_free_params.keys()},
-    'mito':{model:{sampler:None for sampler in samplers} for model in models_free_params.keys()}
-}
-
 for i, model in enumerate(models_free_params.keys()):
     
     print(f"Processing model {i+1}: {model}")
@@ -76,65 +69,60 @@ for i, model in enumerate(models_free_params.keys()):
         idata_lyso = az.from_netcdf(data_dir + model + '_lyso_mcmc_samples_' + sampler + '.nc')
         idata_mito = az.from_netcdf(data_dir + model + '_mito_mcmc_samples_' + sampler + '.nc')
 
-        # store idata
-        idata_dict['cyto'][model][sampler] = idata_cyto
-        idata_dict['lyso'][model][sampler] = idata_lyso
-        idata_dict['mito'][model][sampler] = idata_mito
+        # # ########### plot traces
+        # az.plot_trace(idata_cyto)
+        # plt.savefig(save_dir + 'cyto_trace_' + sampler + '.png', dpi=500)
+        # az.plot_trace(idata_lyso)
+        # plt.savefig(save_dir + 'lyso_trace_' + sampler + '.png', dpi=500)
+        # az.plot_trace(idata_mito)
+        # plt.savefig(save_dir + 'mito_trace_' + sampler + '.png', dpi=500)
 
-        # ########### plot traces
-        az.plot_trace(idata_cyto)
-        plt.savefig(save_dir + 'cyto_trace_' + sampler + '.png', dpi=500)
-        az.plot_trace(idata_lyso)
-        plt.savefig(save_dir + 'lyso_trace_' + sampler + '.png', dpi=500)
-        az.plot_trace(idata_mito)
-        plt.savefig(save_dir + 'mito_trace_' + sampler + '.png', dpi=500)
+        # ########### convergence metrics
+        # with open(model + '_convergence_' + sampler + '.txt', 'w') as file:
+        #     summary = az.summary(idata_cyto)
+        #     file.write("Cytosol:\n")
+        #     file.write(summary.to_string())
+        #     file.write("\n\n")
 
-        ########### convergence metrics
-        with open(model + '_convergence_' + sampler + '.txt', 'w') as file:
-            summary = az.summary(idata_cyto)
-            file.write("Cytosol:\n")
-            file.write(summary.to_string())
-            file.write("\n\n")
+        #     summary = az.summary(idata_lyso)
+        #     file.write("Lysosome:\n")
+        #     file.write(summary.to_string())
+        #     file.write("\n\n")
 
-            summary = az.summary(idata_lyso)
-            file.write("Lysosome:\n")
-            file.write(summary.to_string())
-            file.write("\n\n")
+        #     summary = az.summary(idata_mito)
+        #     file.write("Mitochondria:\n")
+        #     file.write(summary.to_string())
+        #     file.write("\n\n")
 
-            summary = az.summary(idata_mito)
-            file.write("Mitochondria:\n")
-            file.write(summary.to_string())
-            file.write("\n\n")
-
-        ############ plot posterior predictive for each model
-        dat = {
-            'cyto':{'idata': idata_cyto, 'data': cyto_data, 'times': cyto_times, 'color': cyto_color},
-            'lyso':{'idata': idata_lyso, 'data': lyso_data, 'times': lyso_times, 'color': lyso_color},
-            'mito':{'idata': idata_mito, 'data': mito_data, 'times': mito_times, 'color': mito_color}
-        }
-        for comp in dat.keys():
-            fig, ax = get_sized_fig_ax(2,1)
+        # ############ plot posterior predictive for each model
+        # dat = {
+        #     'cyto':{'idata': idata_cyto, 'data': cyto_data, 'times': cyto_times, 'color': cyto_color},
+        #     'lyso':{'idata': idata_lyso, 'data': lyso_data, 'times': lyso_times, 'color': lyso_color},
+        #     'mito':{'idata': idata_mito, 'data': mito_data, 'times': mito_times, 'color': mito_color}
+        # }
+        # for comp in dat.keys():
+        #     fig, ax = get_sized_fig_ax(2,1)
     
-            fig, ax, leg = plot_predictive(dat[comp]['idata'], dat[comp]['data'], dat[comp]['times'], 
-                            plot_prior=False, add_t_0=True, n_traces=0, figsize=None, 
-                            prior_color='', post_color=dat[comp]['color'], data_color=dat[comp]['color'], 
-                            data_marker_size=10, fig_ax = (fig, ax))
+        #     fig, ax, leg = plot_predictive(dat[comp]['idata'], dat[comp]['data'], dat[comp]['times'], 
+        #                     plot_prior=False, add_t_0=True, n_traces=0, figsize=None, 
+        #                     prior_color='', post_color=dat[comp]['color'], data_color=dat[comp]['color'], 
+        #                     data_marker_size=10, fig_ax = (fig, ax))
         
-            # add n_trajectories to the plot if n_trajectories > 0
-            if n_trajectories > 0:
-                for i in range(n_trajectories):
-                    ax.plot(dat[comp]['times'], 
-                        jnp.squeeze(dat[comp]['idata'].posterior_predictive["llike"][0,i,:].values), 
-                        color=dat[comp]['color'], alpha=0.2, linewidth=1.0)
+        #     # add n_trajectories to the plot if n_trajectories > 0
+        #     if n_trajectories > 0:
+        #         for i in range(n_trajectories):
+        #             ax.plot(dat[comp]['times'], 
+        #                 jnp.squeeze(dat[comp]['idata'].posterior_predictive["llike"][0,i,:].values), 
+        #                 color=dat[comp]['color'], alpha=0.2, linewidth=1.0)
                     
-            export_legend(leg, save_dir + f'{comp}_ppc_legend_' + sampler + '.pdf')
-            leg.remove()
+        #     export_legend(leg, save_dir + f'{comp}_ppc_legend_' + sampler + '.pdf')
+        #     leg.remove()
                    
-            ax.set_xlabel("")
-            ax.set_ylabel("")
-            ax.set_ylim(0, 1.5)
+        #     ax.set_xlabel("")
+        #     ax.set_ylabel("")
+        #     ax.set_ylim(0, 1.5)
 
-            plt.savefig(save_dir + f'{comp}_ppc_' + sampler + '.pdf', transparent=True, bbox_inches='tight')
+        #     plt.savefig(save_dir + f'{comp}_ppc_' + sampler + '.pdf', transparent=True, bbox_inches='tight')
 
 
         ############ plot posterior for each model
@@ -217,57 +205,158 @@ for i, model in enumerate(models_free_params.keys()):
                 'CaMKK_KD':post_sims_CaMKK_KD
             }
 
-        ############ plots
-        for comp in dat.keys():
-            for perturb in ['baseline', 'LKB1_KD', 'CaMKK_KD']:
-                # baseline
-                fig_width, fig_height = 1.75, 0.5
-                fig, ax = get_sized_fig_ax(fig_width, fig_height)
+        # ############ plots
+        # for comp in dat.keys():
+        #     for perturb in ['baseline', 'LKB1_KD', 'CaMKK_KD']:
+        #         ####### posterior #######
+        #         fig_width, fig_height = 1.25, 0.5
+        #         fig, ax = get_sized_fig_ax(fig_width, fig_height)
 
-                if perturb == 'baseline':
-                    data = dat[comp]['data']
-                else:
-                    data = np.nan*np.ones_like(dat[comp]['data'])
+        #         if perturb == 'baseline':
+        #             data = dat[comp]['data']
+        #             linestyle = '-'
+        #         else:
+        #             # no data plotted for perturbed conditions
+        #             data = np.nan*np.ones_like(dat[comp]['data'])
+        #             if perturb == 'LKB1_KD':
+        #                 linsetstyle = '--'
+        #             else:
+        #                 linestyle = ':'
         
-                fig, ax, leg = plot_predictive(sims[comp][perturb], data, dat[comp]['times'], 
-                                plot_prior=False, add_t_0=True, n_traces=0, figsize=None, 
-                                prior_color='', post_color=dat[comp]['color'], data_color=dat[comp]['color'], 
-                                data_marker_size=10, fig_ax = (fig, ax))
+        #         fig, ax, leg = plot_predictive(sims[comp][perturb], data, dat[comp]['times'], 
+        #                         plot_prior=False, add_t_0=True, n_traces=0, figsize=None, 
+        #                         prior_color='', post_color=dat[comp]['color'], data_color=dat[comp]['color'], 
+        #                         data_marker_size=10, fig_ax = (fig, ax))
             
-                # add n_trajectories to the plot if n_trajectories > 0
-                if n_trajectories > 0:
-                    for i in range(n_trajectories):
-                        ax.plot(dat[comp]['times'], 
-                            jnp.squeeze(sims[comp][perturb][i,:]), 
-                            color=dat[comp]['color'], alpha=0.2, linewidth=1.0)
+        #         # add n_trajectories to the plot if n_trajectories > 0
+        #         if n_trajectories > 0:
+        #             for i in range(n_trajectories):
+        #                 ax.plot(dat[comp]['times'], 
+        #                     jnp.squeeze(sims[comp][perturb][i,:]), 
+        #                     color=dat[comp]['color'], alpha=0.2, linewidth=1.0)
                         
-                export_legend(leg, save_dir + f'{comp}_ppc_legend_' + sampler + '.pdf')
-                leg.remove()
+        #         export_legend(leg, save_dir + f'{comp}_ppc_legend_' + sampler + '.pdf')
+        #         leg.remove()
                     
-                ax.set_xlabel("time (min)", fontsize=10.0)
-                ax.set_ylabel("")
-                ax.set_ylim(0, 1.5)
+        #         ax.set_xlabel("time (min)", fontsize=10.0)
+        #         ax.set_ylabel("")
+        #         ax.set_ylim(0, 1.5)
 
-                if perturb == 'baseline':
-                    plt.savefig(save_dir + f'{comp}_posterior_{sampler}.pdf', 
-                                transparent=True, bbox_inches='tight')
-                else:
-                    plt.savefig(save_dir + f'{comp}_posterior_{perturb}_{sampler}.pdf', 
-                                transparent=True, bbox_inches='tight')
+        #         if perturb == 'baseline':
+        #             plt.savefig(save_dir + f'{comp}_posterior_{sampler}.pdf', 
+        #                         transparent=True, bbox_inches='tight')
+        #         else:
+        #             plt.savefig(save_dir + f'{comp}_posterior_{perturb}_{sampler}.pdf', 
+        #                         transparent=True, bbox_inches='tight')
+                    
+    ####### t-half-max and change in max activation #######
+    # t-half-max
+    # get t_half_max for each condition
 
-# ############ compare models using ELPD
+    # calculate time to half max of pAMPKAR_stressed/AMPKAR_stressed
+    def compute_half_max(arr):
+        half_max = arr.max() / 2
+        half_max_idx = np.argmin(np.abs(arr - half_max))
+        return half_max_idx
+    
+    t_half_max = {
+        'cyto':{'baseline':None,'LKB1_KD':None,'CaMKK_KD':None},
+        'lyso':{'baseline':None,'LKB1_KD':None,'CaMKK_KD':None},
+        'mito':{'baseline':None,'LKB1_KD':None,'CaMKK_KD':None}
+    }
 
-# # repeat for each sampler
-# for sampler in samplers:
-#     cyto_dat = {model:idata_dict['cyto'][model][sampler] for model in idata_dict['cyto'].keys()}
-#     lyso_dat = {model:idata_dict['lyso'][model][sampler] for model in idata_dict['lyso'].keys()}
-#     mito_dat = {model:idata_dict['mito'][model][sampler] for model in idata_dict['mito'].keys()}
+    max_dat = {
+        'cyto':{'baseline':None,'LKB1_KD':None,'CaMKK_KD':None},
+        'lyso':{'baseline':None,'LKB1_KD':None,'CaMKK_KD':None},
+        'mito':{'baseline':None,'LKB1_KD':None,'CaMKK_KD':None}
+    }
 
-#     # run az.compare
-#     cyto_compare = az.compare(cyto_dat)
-#     lyso_compare = az.compare(lyso_dat)
-#     mito_compare = az.compare(mito_dat)
+    for comp in ['cyto', 'lyso', 'mito']:
+        t_half_idx = np.apply_along_axis(compute_half_max, 1, sims[comp]['baseline'])
+        t_half_max[comp]['baseline'] = [dat[comp]['times'][idx] for idx in t_half_idx]
+        t_half_idx = np.apply_along_axis(compute_half_max, 1, sims[comp]['LKB1_KD'])
+        t_half_max[comp]['LKB1_KD'] = [dat[comp]['times'][idx] for idx in t_half_idx]
+        t_half_idx = np.apply_along_axis(compute_half_max, 1, sims[comp]['CaMKK_KD'])
+        t_half_max[comp]['CaMKK_KD'] = [dat[comp]['times'][idx] for idx in t_half_idx]
 
-#     print(cyto_compare)
-#     print(lyso_compare)
-#     print(mito_compare)
+        max_dat[comp]['baseline'] = sims[comp]['baseline'][:,-1]
+        max_dat[comp]['LKB1_KD'] = sims[comp]['LKB1_KD'][:,-1]
+        max_dat[comp]['CaMKK_KD'] = sims[comp]['CaMKK_KD'][:,-1]
+
+    # plot t_half_max
+    fig, ax = get_sized_fig_ax(1.5, 1.0)
+
+    thalf_df = pd.DataFrame(t_half_max)
+    thalf_df = thalf_df.melt(var_name='compartment', value_name='t_half_max', ignore_index=False)
+    thalf_df = thalf_df.explode('t_half_max')
+    thalf_df = thalf_df.reset_index()
+    thalf_df = thalf_df.rename(columns={'index':'perturbation'})
+
+    thalf_df['compartment'] = thalf_df['compartment'].replace('cyto', 'cytoplasm')
+    thalf_df['compartment'] = thalf_df['compartment'].replace('lyso', 'lysosome')
+    thalf_df['compartment'] = thalf_df['compartment'].replace('mito', 'mitochondria')
+
+    sns.barplot(data=thalf_df, x='perturbation', y='t_half_max', hue='compartment', 
+                ax=ax, palette=colors, linewidth=0, edgecolor='black',width=0.8,
+                dodge=True, saturation=1.0, gap=0.1)
+    
+    for patch in ax.patches:
+        face_color = patch.get_facecolor()
+        # Apply transparency to the face color only
+        patch.set_facecolor(mpl.colors.to_rgba(face_color, alpha=0.5))  
+        patch.set_edgecolor(face_color)  # Set edge color to match the fill
+
+    leg = ax.legend(title='', loc='upper left', bbox_to_anchor=(3, 1), 
+                    fontsize=8.0, ncols=2)
+    export_legend(leg, save_dir + f't_half_max_legend.pdf')
+    leg.remove()
+
+    ax.set_xticklabels(['WT', 'LKB1 KD', 'CaMKK KD'], fontsize=8.0, rotation=45, 
+                       )
+    
+    ax.set_yticks([0, 10])
+    ax.set_yticklabels([0, 10], fontsize=8.0)
+
+    ax.set_ylabel(r"$T_{1/2}$ (min)", fontsize=10.0)
+    ax.set_xlabel("")
+
+    plt.savefig(save_dir + f'{model}_t_half_max.pdf', transparent=True, bbox_inches='tight')
+
+    # plot max activation
+    fig, ax = get_sized_fig_ax(1.5, 1.0)
+
+    max_df = pd.DataFrame(max_dat)
+    max_df = max_df.melt(var_name='compartment', value_name='max', ignore_index=False)
+    max_df = max_df.explode('max')
+    max_df = max_df.reset_index()
+    max_df = max_df.rename(columns={'index':'perturbation'})
+    
+
+    sns.barplot(data=max_df, x='perturbation', y='max', hue='compartment', 
+                ax=ax, palette=colors, linewidth=0, edgecolor='black',width=0.8,
+                dodge=True, saturation=1.0, gap=0.1)
+    
+    for patch in ax.patches:
+        face_color = patch.get_facecolor()
+        # Apply transparency to the face color only
+        patch.set_facecolor(mpl.colors.to_rgba(face_color, alpha=0.5))  
+        patch.set_edgecolor(mpl.colors.to_rgba(face_color, alpha=1.0))  # Set edge color to match the fill
+
+    # leg = ax.legend(title='', loc='upper left', bbox_to_anchor=(3, 1), fontsize=8.0)
+    # export_legend(leg, save_dir + f't_half_max_legend.pdf')
+    ax.get_legend().remove()
+    # leg.remove()
+
+
+    ax.set_xticklabels(['WT', 'LKB1 KD', 'CaMKK KD'], fontsize=8.0, rotation=45, 
+                       )
+    
+    ax.set_yticks([0, 1])
+    ax.set_yticklabels([0, 1.0], fontsize=8.0)
+    ax.set_ylim(0, 1.15)
+
+    ax.set_ylabel("max. fraction\nactive sensor", fontsize=10.0)
+    ax.set_xlabel("")
+
+    plt.savefig(save_dir + f'{model}_max_act.pdf', transparent=True, bbox_inches='tight')
+                
