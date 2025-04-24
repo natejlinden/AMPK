b7e3e2f774040e40828c488a77ae17463e482e74
Nathaniel Linden <nlinden@ucsd.edu>
2025-04-24 06:43:25 -0700

    ELPD of cyto only and compute total (sum) over WT and kinase KO

--- src/ampk_models/param_est/model_selection_kinase_KO.py
+++ src/ampk_models/param_est/model_selection_kinase_KO.py
@@ -23,165 +23,200 @@ jax.config.update("jax_enable_x64", True)
 
 plt.style.use('custom')
 mpl.rcParams['figure.autolayout'] = True
-colors = mb.met_brew(name="Veronese", n=7)
-colors = colors[0:4]
+colors = mb.met_brew(name="Egypt", n=4)
 
 print(colors)
 
-samplers = ["Pathfinder"] #,"Nutpie", "NUTS"] # list of samplers used
-n_trajectories = 10 # number of trajectories to add to PPC plot
-
-cyto_color = colors[0]
-lyso_color = colors[1]
-mito_color = colors[2]
-
+samplers = ["Pathfinder"] # list of samplers used
 
 models_free_params = { 
-        # "ampk_Coccimiglio": {'info_file': '../models/ampk_Coccimiglio.json'},
-        "MA_single": {'info_file':'../models/MA_single.json'},
-        # "MM_single":  {'info_file': '../models/MM_single.json'},
-        "MA_nonessential": {'info_file': '../models/MA_nonessential.json'},
-        "MM_nonessential":  {'info_file': '../models/MM_nonessential.json'}
+        "MA_single": {'info_file':'../models/MA_single.json', 'name':'1'},
+        "MM_single":  {'info_file': '../models/MM_single.json', 'name':'2'},
+        "MA_nonessential": {'info_file': '../models/MA_nonessential.json', 'name':'3'},
+        "MM_nonessential":  {'info_file': '../models/MM_nonessential.json', 'name':'4'},
+        "MA_nonessential_all": {'info_file': '../models/MA_nonessential_all.json', 'name':'5'},
+        "MM_nonessential_all":  {'info_file': '../models/MM_nonessential_all.json', 'name':'6'}
         }
 
 data_dir = '../../../results/param_est/kinase_KO/std_dcr/'
 save_dir_base = '../../../results/param_est/kinase_KO/std_dcr/figs/'
 
 # dictionary to store idata
-idata_dict = {
-    'cyto':{model:{sampler:None for sampler in samplers} for model in models_free_params.keys()},
-    'lyso':{model:{sampler:None for sampler in samplers} for model in models_free_params.keys()},
-    'mito':{model:{sampler:None for sampler in samplers} for model in models_free_params.keys()}
-}
-
-llike_dict = {
-    'cyto':{model:{sampler:None for sampler in samplers} for model in models_free_params.keys()},
-    'lyso':{model:{sampler:None for sampler in samplers} for model in models_free_params.keys()},
-    'mito':{model:{sampler:None for sampler in samplers} for model in models_free_params.keys()}
-}
+idata_dict_cyto = {model:{sampler:None for sampler in samplers} for model in models_free_params.keys()}
+llike_dict_cyto = {model:{sampler:None for sampler in samplers} for model in models_free_params.keys()}
 
 for i, model in enumerate(models_free_params.keys()):
     
     print(f"Processing model {i+1}: {model}")
 
     for sampler in samplers:
-    
         save_dir = save_dir_base + model + '/'
         if not os.path.exists(save_dir):
             os.makedirs(save_dir)
 
         # load the llike idata
         idata_cyto = az.from_netcdf(data_dir + model + '_cyto_mcmc_samples_llike_' + sampler + '.nc')
-        idata_lyso = az.from_netcdf(data_dir + model + '_lyso_mcmc_samples_llike_' + sampler + '.nc')
-        idata_mito = az.from_netcdf(data_dir + model + '_mito_mcmc_samples_llike_' + sampler + '.nc')
 
         # store idata
-        idata_dict['cyto'][model][sampler] = idata_cyto
-        idata_dict['lyso'][model][sampler] = idata_lyso
-        idata_dict['mito'][model][sampler] = idata_mito
-
+        idata_dict_cyto[model][sampler] = idata_cyto
 ############ compare models using ELPD
+
+print(idata_cyto['log_likelihood'])
+
+llike_names = {'llike_WT':'Wild type', 
+               'llike_LKB1_KO':'LKB1_KO', 
+               'llike_CaMKK2_KO':'CaMKK2_KO'}
 
 # repeat for each sampler
 for sampler in samplers:
-    elpd = {}
-    for llike_name in ['llike', 'llike_LKB1_KO', 'llike_CaMKK2_KO']:
-        cyto_dat = {model:idata_dict['cyto'][model][sampler] for model in idata_dict['cyto'].keys()}
-        lyso_dat = {model:idata_dict['lyso'][model][sampler] for model in idata_dict['lyso'].keys()}
-        mito_dat = {model:idata_dict['mito'][model][sampler] for model in idata_dict['mito'].keys()}
+    elpds = []
+    for llike_name in ['llike_WT', 'llike_LKB1_KO', 'llike_CaMKK2_KO']:
+        cyto_dat = {model:idata_dict_cyto[model][sampler] for model in idata_dict_cyto.keys()}
 
         # run az.compare
         cyto_compare = az.compare(cyto_dat, var_name=llike_name)
-        lyso_compare = az.compare(lyso_dat, var_name=llike_name)
-        mito_compare = az.compare(mito_dat, var_name=llike_name)
 
-        # combine into one dataframe with compartment as a column
-        cyto_compare['compartment'] = 'cyto'
-        lyso_compare['compartment'] = 'lyso'
-        mito_compare['compartment'] = 'mito'
+        cyto_compare = cyto_compare[['elpd_loo']].reset_index()
+        cyto_compare.rename(columns={'index': 'model'}, inplace=True)
+        cyto_compare['llike'] = llike_names[llike_name]  # Add a new column for 'llike'
 
-        compare = pd.concat([cyto_compare, lyso_compare, mito_compare]).loc[:, ['elpd_loo', 'compartment']].reset_index()
-        compare = compare.rename(columns={'index': 'model'})
+        # store elpds
+        elpds.append(cyto_compare)    
+    # combine into one dataframe
+    elpd = pd.concat(elpds, ignore_index=True)
 
-        elpd[llike_name] = compare
+    # Calculate the sum of ELPD for each model over the unique values in llike
+    elpd_sum = elpd.groupby('model')['elpd_loo'].sum().reset_index()
+    elpd_sum['llike'] = 'Total ELPD'  # Add a new column for 'all'
 
-        # make a plot
-        fig, ax = get_sized_fig_ax(1.5, 2)
+    # Append the summed ELPD as a new row for each model
+    elpd = pd.concat([elpd, elpd_sum], ignore_index=True)
 
-        ax.axhline(0, color='black', linewidth=0.5, linestyle='--')
+    # Replace model names in the 'model' column with their corresponding 'name' from models_free_params
+    elpd['model'] = elpd['model'].map(lambda x: models_free_params[x]['name'])
 
-        sns.barplot(data=compare, x='compartment', y='elpd_loo', hue='model', ax=ax,
-                    palette=colors, linewidth=1,
-                    edgecolor='black')
+    print(elpd)
 
-        for patch in ax.patches:
-            face_color = patch.get_facecolor()
-            # Apply transparency to the face color only
-            patch.set_facecolor(mpl.colors.to_rgba(face_color, alpha=0.8))  
+    # Ensure the models are ordered according to the keys in models_free_params
+    elpd['model'] = pd.Categorical(elpd['model'], 
+            categories=[models_free_params[mod]['name'] for mod in list(models_free_params.keys())], 
+            ordered=True)
 
-            patch.set_edgecolor(face_color)  # Set edge color to match the fill
 
-        # Create Custom Legend Handles to Match Bar Styles
-        handles = []
-        # unique_groups = ['Michaelis-Menten (MM)', 'MM nonessential', 'Mass action (MA)', 'MA nonessential']
-        unique_groups = ['MM nonessential', 'Mass action (MA)', 'MA nonessential']
-        palette = colors
+    # Create a clustered bar plot using seaborn
+    fig, ax = get_sized_fig_ax(width=4, height=2.)
 
-        for color, group in zip(palette, unique_groups):
-            handles.append(mpl.patches.Patch(facecolor=mpl.colors.to_rgba(color, alpha=0.8), 
-                                            edgecolor=color, linewidth=1., label=group))
+    sns.barplot(data=elpd, x='model', y='elpd_loo', hue='llike', ax=ax,
+                palette=colors[:elpd['llike'].nunique()], edgecolor='black', alpha=0.8)
 
-        # Apply the Custom Legend
-        leg = ax.legend(handles=handles, loc='upper left', bbox_to_anchor=(1.5, 1),
-                fontsize=8, title_fontsize=10)
-        export_legend(leg, save_dir_base + 'elpd_legend.pdf')
-        leg.remove()
-
-        ax.set_ylabel('expected log pointwise\npredictive density\n(predictive accuracy)', fontsize=10)
-        ax.set_xlabel('sub-cellular compartment', fontsize=10)
-        ax.tick_params(axis='both', which='major', labelsize=8)
-        ax.set_xticklabels(['cytoplasm', 'lysosome', 'mitochondria'])
-
-        fig.savefig(save_dir_base + 'elpd_' + sampler +'_' + llike_name + '.pdf', bbox_inches='tight',
-                    transparent=True)
-        
-    # average ELPD over three datasets
-    elpd_combined = pd.concat([df.assign(llike=key) for key, df in elpd.items()])
-
-    elpd_combined_avg = elpd_combined.groupby(['model', 'compartment'])['elpd_loo'].mean().reset_index()
-    elpd_combined_avg = elpd_combined_avg.rename(columns={'elpd_loo': 'avg_elpd_loo'})
-    print(elpd_combined_avg)
-
-    fig, ax = get_sized_fig_ax(2.5, 2)
-
+    # Customize the plot
     ax.axhline(0, color='black', linewidth=0.5, linestyle='--')
+    ax.set_ylabel('ELPD\n(Expected Log Predictive Density)', fontsize=10)
+    ax.set_xlabel('Model', fontsize=10)
+    ax.tick_params(axis='both', which='major', labelsize=8)
 
-    sns.barplot(data=elpd_combined_avg, x='compartment', y='avg_elpd_loo', hue='model', ax=ax,
-                palette=colors, linewidth=1,
-                edgecolor='black')
+    leg = ax.legend(fontsize=8, loc='upper left', bbox_to_anchor=(1.25, 1),
+                    ncols=4)
 
-    for patch in ax.patches:
-        face_color = patch.get_facecolor()
-        patch.set_facecolor(mpl.colors.to_rgba(face_color, alpha=0.8))  
-        patch.set_edgecolor(face_color)
-
-    handles = []
-    unique_groups = ['MM nonessential', 'Mass action (MA)', 'MA nonessential']
-    palette = colors
-
-    for color, group in zip(palette, unique_groups):
-        handles.append(mpl.patches.Patch(facecolor=mpl.colors.to_rgba(color, alpha=0.8), 
-                                        edgecolor=color, linewidth=1., label=group))
-
-    leg = ax.legend(handles=handles, loc='upper left', bbox_to_anchor=(1.5, 1),
-            fontsize=8, title_fontsize=10)
-    export_legend(leg, save_dir_base + 'elpd_combined_legend.pdf')
+    export_legend(leg, save_dir_base + f'elpd_legend_{sampler}.pdf')
     leg.remove()
 
-    ax.set_ylabel('expected log pointwise\npredictive density\n(predictive accuracy)', fontsize=10)
-    ax.set_xlabel('sub-cellular compartment', fontsize=10)
-    ax.tick_params(axis='both', which='major', labelsize=8)
-    ax.set_xticklabels(['cytoplasm', 'lysosome', 'mitochondria'])
+    fig.savefig(save_dir_base + f'elpd_{sampler}.pdf', bbox_inches='tight', transparent=True)
 
-    fig.savefig(save_dir_base + 'elpd_combined_' + sampler + '.pdf', bbox_inches='tight',
-                transparent=True)
+    # # Save the plot
+    # fig.tight_layout()
+    # fig.savefig(save_dir_base + f'clustered_bar_elpd_{sampler}.pdf', bbox_inches='tight', transparent=True)
+    # plt.close(fig)
+
+    # # Pivot the data to prepare for stacking
+    # elpd_pivot = elpd.pivot(index='model', columns='llike', values='elpd_loo').fillna(0)
+
+
+    #     # combine into one dataframe with compartment as a column
+    #     cyto_compare['compartment'] = 'cyto'
+
+    #     compare = pd.concat([cyto_compare]).loc[:, ['elpd_loo', 'compartment']].reset_index()
+    #     compare = compare.rename(columns={'index': 'model'})
+
+    #     elpd[llike_name] = compare
+
+    #     # make a plot
+    #     fig, ax = get_sized_fig_ax(1.5, 2)
+
+    #     ax.axhline(0, color='black', linewidth=0.5, linestyle='--')
+
+    #     sns.barplot(data=compare, x='compartment', y='elpd_loo', hue='model', ax=ax,
+    #                 palette=colors, linewidth=1,
+    #                 edgecolor='black')
+
+    #     for patch in ax.patches:
+    #         face_color = patch.get_facecolor()
+    #         # Apply transparency to the face color only
+    #         patch.set_facecolor(mpl.colors.to_rgba(face_color, alpha=0.8))  
+
+    #         patch.set_edgecolor(face_color)  # Set edge color to match the fill
+
+    #     # Create Custom Legend Handles to Match Bar Styles
+    #     handles = []
+    #     # unique_groups = ['Michaelis-Menten (MM)', 'MM nonessential', 'Mass action (MA)', 'MA nonessential']
+    #     unique_groups = ['MM nonessential', 'Mass action (MA)', 'MA nonessential']
+    #     palette = colors
+
+    #     for color, group in zip(palette, unique_groups):
+    #         handles.append(mpl.patches.Patch(facecolor=mpl.colors.to_rgba(color, alpha=0.8), 
+    #                                         edgecolor=color, linewidth=1., label=group))
+
+    #     # Apply the Custom Legend
+    #     leg = ax.legend(handles=handles, loc='upper left', bbox_to_anchor=(1.5, 1),
+    #             fontsize=8, title_fontsize=10)
+    #     export_legend(leg, save_dir_base + 'elpd_legend.pdf')
+    #     leg.remove()
+
+    #     ax.set_ylabel('expected log pointwise\npredictive density\n(predictive accuracy)', fontsize=10)
+    #     ax.set_xlabel('sub-cellular compartment', fontsize=10)
+    #     ax.tick_params(axis='both', which='major', labelsize=8)
+    #     ax.set_xticklabels(['cytoplasm', 'lysosome', 'mitochondria'])
+
+    #     fig.savefig(save_dir_base + 'elpd_' + sampler +'_' + llike_name + '.pdf', bbox_inches='tight',
+    #                 transparent=True)
+        
+    # # average ELPD over three datasets
+    # elpd_combined = pd.concat([df.assign(llike=key) for key, df in elpd.items()])
+
+    # elpd_combined_avg = elpd_combined.groupby(['model', 'compartment'])['elpd_loo'].mean().reset_index()
+    # elpd_combined_avg = elpd_combined_avg.rename(columns={'elpd_loo': 'avg_elpd_loo'})
+    # print(elpd_combined_avg)
+
+    # fig, ax = get_sized_fig_ax(2.5, 2)
+
+    # ax.axhline(0, color='black', linewidth=0.5, linestyle='--')
+
+    # sns.barplot(data=elpd_combined_avg, x='compartment', y='avg_elpd_loo', hue='model', ax=ax,
+    #             palette=colors, linewidth=1,
+    #             edgecolor='black')
+
+    # for patch in ax.patches:
+    #     face_color = patch.get_facecolor()
+    #     patch.set_facecolor(mpl.colors.to_rgba(face_color, alpha=0.8))  
+    #     patch.set_edgecolor(face_color)
+
+    # handles = []
+    # unique_groups = ['MM nonessential', 'Mass action (MA)', 'MA nonessential']
+    # palette = colors
+
+    # for color, group in zip(palette, unique_groups):
+    #     handles.append(mpl.patches.Patch(facecolor=mpl.colors.to_rgba(color, alpha=0.8), 
+    #                                     edgecolor=color, linewidth=1., label=group))
+
+    # leg = ax.legend(handles=handles, loc='upper left', bbox_to_anchor=(1.5, 1),
+    #         fontsize=8, title_fontsize=10)
+    # export_legend(leg, save_dir_base + 'elpd_combined_legend.pdf')
+    # leg.remove()
+
+    # ax.set_ylabel('expected log pointwise\npredictive density\n(predictive accuracy)', fontsize=10)
+    # ax.set_xlabel('sub-cellular compartment', fontsize=10)
+    # ax.tick_params(axis='both', which='major', labelsize=8)
+    # ax.set_xticklabels(['cytoplasm', 'lysosome', 'mitochondria'])
+
+    # fig.savefig(save_dir_base + 'elpd_combined_' + sampler + '.pdf', bbox_inches='tight',
+    #             transparent=True)
